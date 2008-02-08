eac97d6f02745943301169bd4810934f65380524
matt <matt@localhost>
2008-02-08 15:53:08 +0000

    Added ability and controls to connect as client. Removed \n between messages in the chat window.

--- belleric/view.cpp
+++ belleric/view.cpp
@@ -5,6 +5,9 @@
 #include <QTcpSocket>
 #include <QByteArray>
 #include <QString>
+#include <QSpinBox>
+#include <QGroupBox>
+#include <QPushButton>
 #include <iostream>
 #include "view.h"
 
@@ -12,12 +15,35 @@ ChatWindow::ChatWindow(QWidget *parent)
 	: QWidget(parent)
 {
 	// set up ui
+	hostLine = new QLineEdit("localhost", this);
+
+	portSpin = new QSpinBox(this);
+	portSpin->setRange(1, 65000); //tune me
+	//portSpin->setPrefix("port ");
+	portSpin->setValue(1337);
+
 	messageEdit = new QLineEdit(QString("talk here"), this);
 	connect(
 		messageEdit, SIGNAL(returnPressed()),
 		this, SLOT(sendMessage()));
+
 	chatText = new QTextEdit(this);
+	chatText->setReadOnly(true);
+
+	connectButton = new QPushButton("Connect", this);
+	QObject::connect(
+		connectButton, SIGNAL(clicked()),
+		this, SLOT(attemptConnection()));
+
+	QGroupBox *connectGroup = new QGroupBox("Connect as client");
+	QHBoxLayout *connectHBox = new QHBoxLayout(connectGroup);
+	connectHBox->addWidget(hostLine);
+	connectHBox->addWidget(portSpin);
+	connectHBox->addWidget(connectButton);
+	connectGroup->setLayout(connectHBox);
+
 	QVBoxLayout *layout = new QVBoxLayout(this);
+	layout->addWidget(connectGroup);
 	layout->addWidget(chatText);
 	layout->addWidget(messageEdit);
 	setLayout(layout);
@@ -27,6 +53,7 @@ ChatWindow::ChatWindow(QWidget *parent)
 	connect(
 		chatServer, SIGNAL(newConnection()),
 		this, SLOT(receiveConnection()));
+	clientSocket = 0;
 }
 
 void ChatWindow::receiveConnection()
@@ -42,21 +69,37 @@ void ChatWindow::receiveConnection()
 		this, SLOT(receiveMessage()));
 }
 
+void ChatWindow::attemptConnection()
+{
+	std::cout << "attempting to connect" << std::endl;
+	//close server if open
+	//try to open socket to given host/port
+	clientSocket = new QTcpSocket(this);
+	clientSocket->connectToHost(hostLine->text(), portSpin->value());
+	connect(
+		clientSocket, SIGNAL(readyRead()),
+		this, SLOT(receiveMessage()));
+}
+
 void ChatWindow::receiveMessage()
 {
 	std::cout << "there are bytes to be read" << std::endl;
 	QByteArray message = clientSocket->readAll();
-	chatText->append(message);
+	if (chatText->toPlainText().isEmpty() == false)
+	{
+		chatText->insertPlainText("\n");
+	}
+	chatText->insertPlainText(message);
 }
 
 void ChatWindow::sendMessage()
 {
 	std::cout << "message will be sent" << std::endl;
-	if (clientSocket)
+	std::cout << "clientSocket has value " << clientSocket << std::endl;
+	if (clientSocket != NULL)
 	{
 		QByteArray message;
 		message.append(messageEdit->text());
-		message.append("\n");
 		clientSocket->write(message);
 	}
 	messageEdit->clear();
--- belleric/view.h
+++ belleric/view.h
@@ -7,6 +7,8 @@ class QTcpSocket;
 class QTextEdit;
 class QTcpServer;
 class QLineEdit;
+class QSpinBox;
+class QPushButton;
 
 class ChatWindow : public QWidget
 {
@@ -17,6 +19,7 @@ public:
 
 private slots:
 	void receiveConnection();
+	void attemptConnection();
 	void receiveMessage();
 	void sendMessage();
 
@@ -25,6 +28,9 @@ private:
 	QTcpSocket *clientSocket;
 	QTextEdit *chatText;
 	QLineEdit *messageEdit;
+	QLineEdit *hostLine;
+	QSpinBox *portSpin;
+	QPushButton *connectButton;
 };
 
 #endif
