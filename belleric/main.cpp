df7b742132250a56760723a079b576a8cb8d7f3e
matt <matt@localhost>
2008-02-08 09:41:57 +0000

    initial skeleton import

---
+++ belleric/main.cpp
@@ -0,0 +1,10 @@
+#include <QApplication>
+#include "view.h"
+
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+	ChatWindow chatWindow;
+	chatWindow.show();
+	return app.exec();
+}
---
+++ belleric/view.h
@@ -0,0 +1,12 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+#include <QWidget>
+
+class ChatWindow : public QWidget
+{
+public:
+	ChatWindow(QWidget *parent = 0);
+};
+
+#endif
