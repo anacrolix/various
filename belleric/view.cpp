a7fff0c27c43111e10a196b050b63a31d99e6c6f
matt <matt@localhost>
2008-02-08 12:44:58 +0000

    created basic view, and async server socket

--- belleric/view.cpp
+++ belleric/view.cpp
@@ -1,6 +1,60 @@
+#include <QVBoxLayout>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QByteArray>
+#include <QString>
+#include <iostream>
 #include "view.h"
 
 ChatWindow::ChatWindow(QWidget *parent)
 	: QWidget(parent)
 {
+	// set up ui
+	messageEdit = new QLineEdit(QString("talk here"), this);
+	connect(
+		messageEdit, SIGNAL(returnPressed()),
+		this, SLOT(sendMessage()));
+	chatText = new QTextEdit(this);
+	QVBoxLayout *layout = new QVBoxLayout(this);
+	layout->addWidget(chatText);
+	layout->addWidget(messageEdit);
+	setLayout(layout);
+	// set up server
+	chatServer = new QTcpServer(this);
+	chatServer->listen(QHostAddress::Any, 1337);
+	connect(
+		chatServer, SIGNAL(newConnection()),
+		this, SLOT(receiveConnection()));
+}
+
+void ChatWindow::receiveConnection()
+{
+	std::cout << "something connected" << std::endl;
+	clientSocket = chatServer->nextPendingConnection();
+	connect(
+		clientSocket, SIGNAL(disconnected()),
+		clientSocket, SLOT(deleteLater()));
+	clientSocket->write("hello\n");
+	connect(
+		clientSocket, SIGNAL(readyRead()),
+		this, SLOT(receiveMessage()));
+}
+
+void ChatWindow::receiveMessage()
+{
+	std::cout << "there are bytes to be read" << std::endl;
+	QByteArray message = clientSocket->readAll();
+	chatText->append(message);
+}
+
+void ChatWindow::sendMessage()
+{
+	std::cout << "message will be sent" << std::endl;
+	QByteArray message;
+	message.append(messageEdit->text());
+	message.append("\n");
+	clientSocket->write(message);
+	messageEdit->clear();
 }
