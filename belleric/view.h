ba99882ac931ee1d9bec36dee3e71386f0841269
matt <matt@localhost>
2008-02-08 12:44:58 +0000

    created basic view, and async server socket

--- belleric/view.h
+++ belleric/view.h
@@ -2,11 +2,29 @@
 #define VIEW_H
 
 #include <QWidget>
+class QTcpServer;
+class QTcpSocket;
+class QTextEdit;
+class QTcpServer;
+class QLineEdit;
 
 class ChatWindow : public QWidget
 {
+	Q_OBJECT
+
 public:
 	ChatWindow(QWidget *parent = 0);
+
+private slots:
+	void receiveConnection();
+	void receiveMessage();
+	void sendMessage();
+
+private:
+	QTcpServer *chatServer;
+	QTcpSocket *clientSocket;
+	QTextEdit *chatText;
+	QLineEdit *messageEdit;
 };
 
 #endif
