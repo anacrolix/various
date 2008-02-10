37b2cb01e172ffed3f223dddf2841743e9bf583f
matt <matt@localhost>
2008-02-10 04:59:14 +0000

    server is now functioning and listens

--- belleric/server/server.h
+++ belleric/server/server.h
@@ -1,5 +1,17 @@
-class ChatServer
+#include <QObject>
+
+class QTcpServer;
+class QTcpSocket;
+
+class ChatServer : QObject
 {
+	Q_OBJECT
 public:
 	ChatServer();
+private slots:
+	void newConnection();
+	void newMessage();
+private:
+	QTcpServer *server;
+	QTcpSocket *client;
 };
