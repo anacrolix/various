8fc57d3fe714b4232ad0fb74ff08415c89bb9b8e
matt <matt@localhost>
2008-02-10 04:03:58 +0000

    split project into client and server, and created server Qt skeleton

---
+++ belleric/server/main.cpp
@@ -0,0 +1,9 @@
+#include <QApplication>
+
+#include "server.h"
+
+int main (int argc, char **argv)
+{
+	QApplication app(argc, argv);
+	return app.exec();
+}
---
+++ belleric/server/server.h
@@ -0,0 +1,5 @@
+class ChatServer
+{
+public:
+	ChatServer();
+};
