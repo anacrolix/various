95501414a67642dbda77bc46cd20cedf385ff41c
matt <matt@localhost>
2008-02-10 04:59:14 +0000

    server is now functioning and listens

--- belleric/server/main.cpp
+++ belleric/server/main.cpp
@@ -5,5 +5,6 @@
 int main (int argc, char **argv)
 {
 	QApplication app(argc, argv);
+	ChatServer chatServer;
 	return app.exec();
 }
--- belleric/server/server.cpp
+++ belleric/server/server.cpp
@@ -3,4 +3,22 @@
 
 #include "server.h"
 
-ChatServer::ChatServer() {}
+ChatServer::ChatServer() : QObject()
+{
+	server = new QTcpServer();
+	server->listen(QHostAddress::Any, 1337);
+	connect(server, SIGNAL(newConnection()), this, SLOT(newConnection()));
+}
+
+void ChatServer::newConnection()
+{
+	client = server->nextPendingConnection();
+	client->write("Welcome");
+	connect(client, SIGNAL(readyRead()), this, SLOT(newMessage()));
+}
+
+void ChatServer::newMessage()
+{
+	QByteArray message = client->readAll();
+	client->write("Thank you for your message");
+}
