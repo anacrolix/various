13fa4b87aad8379bc8cc5766924a973b805d3a53
matt <matt@localhost>
2008-02-17 03:40:18 +0000

    wrapped the send calls to tcp_write()

--- msgprot/network.h
+++ msgprot/network.h
@@ -6,5 +6,6 @@ int tcp_listen(unsigned short int port, unsigned int backlog);
 int tcp_accept(int socket);
 char *tcp_peer_host(int socket);
 unsigned short int tcp_peer_port(int socket);
+ssize_t tcp_write(int fd, const void *buf, size_t len);
 
 #endif
