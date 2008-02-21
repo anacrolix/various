d915c3467c17c5fbe9abdbf905cd2673cab303cd
matt <matt@localhost>
2008-02-21 12:19:13 +0000

    functioning with basic command processing

--- msgprot/client.c
+++ msgprot/client.c
@@ -12,36 +12,28 @@ int main()
 {
 	int sock = tcp_connect("localhost", 1337);
 	char zomgbuf[1024];
+	strcpy(zomgbuf, "msg");
 	while (1) {
-		//char zomgbuf[1024];
-		printf("enter message: ");
-		if (EOF == scanf("%1023s", zomgbuf)) break;
-		size_t msgsize = strlen(zomgbuf);
 		struct tcp_msg tm = tcp_msg_new();
-		tcp_msg_init(&tm, sock, msgsize);
-		for (int i = 0; i < msgsize; i++) {
-			sleep(1);
-			tcp_msg_send(&tm, zomgbuf + i, 1);
-			printf("send the \'%c\'\n", *(zomgbuf + i));
+		size_t msgsize;
+		printf("enter message: ");
+		if (EOF == scanf("%1023s", zomgbuf + 4)) {
+			msgsize = strlen("exit") + 1;
+			tcp_msg_init(&tm, sock, msgsize);
+			tcp_msg_send(&tm, "exit", msgsize);
+			tcp_msg_clean(&tm);
+			break;
+		} else {
+			msgsize = strlen(zomgbuf + 4) + 5;
+			tcp_msg_init(&tm, sock, msgsize);
+			for (int i = 0; i < msgsize; i++) {
+				//sleep(1);
+				tcp_msg_send(&tm, zomgbuf + i, 1);
+				printf("send the \'%c\'\n", *(zomgbuf + i));
+			}
+			tcp_msg_clean(&tm);
 		}
-		tcp_msg_clean(&tm);
 	}
 	tcp_close(sock);
-	/*
-	int sock = tcp_connect("localhost", 1337);
-	char zomgbuf[1024];
-	for (;;) {
-		printf("enter message: ");
-		scanf("%s", zomgbuf);
-		size_t msg_len = strlen(zomgbuf);
-		struct tcp_msg tm = tcp_msg_init(sock, msg_len);
-		for (int i = 0; i < msg_len; i++) {
-			sleep(1);
-			tcp_msg_send(&tm, zomgbuf + i, 1);
-			printf("send the \'%c\'\n", *(zomgbuf+i));
-		}
-		tcp_msg_destroy(&tm);
-	}
-	*/
 	return EXIT_SUCCESS;
 }
