de72951d48b0c8b814acd82b8178074beab16a64
matt <matt@localhost>
2008-03-17 09:31:55 +0000

    removed unused functions, hashes files without tags

--- mp3clean/mp3clean.c
+++ mp3clean/mp3clean.c
@@ -3,100 +3,98 @@
 #include <stdlib.h>
 #include <openssl/sha.h>
 #include <string.h>
-//#include <musicbrainz/mb_c.h>
 #include <error.h>
 #include <errno.h>
 #include <assert.h>
-//#include <id3tag.h>
 #include <strings.h>
 #include <stdio.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 typedef enum {no = 0, yes} has_t;
-//typedef __off64_t off_t;
 
-#define fatal(errval, fmt, ...) (error_at_line(EXIT_FAILURE, errval, __FILE__, __LINE__, fmt, ##__VA_ARGS__))
+#define fatal(errval, fmt, ...) \
+	(error_at_line(EXIT_FAILURE, errval, __FILE__, __LINE__, fmt, ##__VA_ARGS__))
+
+#define warn(errval, fmt, ...) \
+	(error_at_line(0, errval, __FILE__, __LINE__, fmt, ##__VA_ARGS__))
 
 #ifdef NDEBUG
-#define warn(errval, fmt, ...) (error_at_line(0, errval, __FILE__, __LINE__, fmt, ##__VA_ARGS__))
+#define debug(errval, fmt, ...)
 #else
-#define warn(errval, fmt, ...) ({printf(fmt, ##__VA_ARGS__); puts(strerror(errval));})
+#define debug(errval, fmt, ...) \
+	(error_at_line(0, errval, __FILE__, __LINE__, fmt, ##__VA_ARGS__))
 #endif
 
-#ifndef NDEBUG
-#define debug(fmt, ...) (fprintf(stderr, fmt, ##__VA_ARGS__))
-#else
-#define debug(fmt, ...)
-#endif
+#define MIN(a, b) ((a < b) ? a : b)
+#define MAX(a, b) ((a > b) ? a : b)
 
-int sha1_file(const char *filename, unsigned char msgdgst[])
+int sha1_file_ex(
+	FILE *fstream,
+	unsigned char msgdgst[],
+	off_t start,
+	off_t end)
 {
-	int ret = 0;
-	
-	FILE *fstream = fopen(filename, "r");
-	if (fstream == NULL) {
-		warn(errno, "fopen(\"%s\", \"r\")", filename);
-		ret = 1;
-	} else {
-		long bufsize = sysconf(_SC_PAGESIZE);
-		assert(sizeof(size_t) >= sizeof(long));
-		void *filedata = malloc(bufsize);
-		if (filedata == NULL) {
+	assert(sizeof(size_t) >= sizeof(long));
+	assert(end >= start && end >= 0 && start >= 0);
+	int ret = 1;
+	void *filedata = NULL;
+	long bufsize = sysconf(_SC_PAGESIZE);
+
+	do {
+		if (fseek(fstream, start, SEEK_SET)) {
+			warn(errno, "fseeko()");
+			break;
+		}
+
+		filedata = malloc(bufsize);
+		if (!filedata) {
 			warn(errno, "malloc()");
-			ret = 1;
-		} else {
-			SHA_CTX shactx;
-			do {
-				if (SHA1_Init(&shactx) != 1) {
-					ret = 1;
-					break;
-				}
-				for (;;) {
-					size_t bytesread = fread(filedata, 1, bufsize, fstream);
-					if (bytesread < bufsize && ferror(fstream)) {
-						warn(errno, "fread()");
-						break;
-					}		
-					if (SHA1_Update(&shactx, filedata, bytesread) != 1) {
-						ret = 1;
-						break;
-					}
-					if (bytesread < bufsize && feof(fstream)) {
-						if (SHA1_Final(msgdgst, &shactx) != 1)
-							ret = 1;
-						break;
-					}
-				}
-			} while (0);
-			free(filedata);
+			break;
 		}
-		int n = fclose(fstream);
-		if (n != 0) {
-			assert(n == EOF);
-			fatal(errno, "fclose()");
-		}
-	}
+
+		SHA_CTX shactx;
+		if (SHA1_Init(&shactx) != 1) break;
+
+		size_t hashcnt = 0;
+		do {
+			size_t reqcnt = MIN(end - start - hashcnt, bufsize);
+			size_t readcnt = fread(filedata, 1, reqcnt, fstream);
+			if (readcnt < reqcnt && (feof(fstream) || ferror(fstream))) {
+				warn(errno, "fread()");
+				break;
+			}
+			assert(readcnt == reqcnt);
+			if (SHA1_Update(&shactx, filedata, readcnt) != 1) break;
+			hashcnt += readcnt;
+		} while (hashcnt < end - start);
+		if (hashcnt < end - start) break;
+		assert(hashcnt == end - start);
+
+		if (SHA1_Final(msgdgst, &shactx) != 1) break;
+
+		ret = 0;
+	} while (0);
+
+	if (filedata) free(filedata);
 	return ret;
 }
 
-has_t has_id3v2(FILE *fs, __off64_t *size)
+has_t get_id3v2(FILE *fs, off_t *size)
 {
+	assert(fs != NULL);
 	has_t ret = no;
 	unsigned char v2hdr[10];
-	
+
 	do {
 		if (fseek(fs, 0, SEEK_SET)) {
 			warn(errno, "fseek()");
 			break;
 		}
-		size_t read = fread(v2hdr, 1, 10, fs);
-		if (read < 10) {
-			if (ferror(fs))
-				warn(errno, "fread()");
-			else if (feof(fs))
-				warn(0, "fread(), file too short?");
-			else
-				assert(0);
+		size_t read = fread(v2hdr, 1, sizeof(v2hdr), fs);
+		if (read < sizeof(v2hdr)) {
+			if (feof(fs) || ferror(fs)) warn(errno, "fread()");
+			else assert(0);
 			break;
 		}
 		// file ident
@@ -107,51 +105,61 @@ has_t has_id3v2(FILE *fs, __off64_t *size)
 			break;
 		// flags
 		assert(v2hdr[5] == 0);
-		if (v2hdr[5] != 0)
+		if (v2hdr[5] != 0) {
+			warn(0, "ID3v2 flag processing is not implemented");
 			break;
-		// 7 bit, bigendian size
-		for (int i = 6; i < 10; i++) {
-			if (v2hdr[i] >> 7 || v2hdr[i] & 0x80) {
-				assert(0);
-				break;
-			}
 		}
+		// 7 bit, bigendian size
+		{
+			int i;
+			for (i = 6; i < 10; i++) {
+				if (v2hdr[i] >> 7 || v2hdr[i] & 0x80) {
+					assert(v2hdr[i] >> 7 && v2hdr[i] & 0x80);
+					break;
+				}
+			}
+			assert(i == 10);
+			if (i != 10) break;
+		}
+		// get 28 bit size
 		if (size != NULL) {
 			*size = 0;
-			assert(sizeof(__off_t) >= sizeof(long));
-			long flagsize = 0;
+			assert(sizeof(off_t) >= sizeof(long));
+			long flagval = 0;
 			for (int i = 0; i < 4; i++) {
-				long incsize = v2hdr[9 - i];
-				incsize = incsize << (i * 7);
-				flagsize |= incsize;
+				long incval = v2hdr[9 - i];
+				incval = incval << (i * 7);
+				flagval |= incval;
 			}
-			*size = flagsize;
+			*size = flagval;
 		}
 		ret = yes;
 	} while (0);
-	
+
 	return ret;
 }
 
-has_t has_id3v1(FILE *fs)
+has_t get_id3v1(FILE *fs)
 {
+	assert(fs != NULL);
 	const static size_t TAG_SIZE = 128;
 	has_t ret = no;
 	char *v1tag = NULL;
-	
+
 	do {
 		if (fseek(fs, -TAG_SIZE, SEEK_END)) {
-			warn(errno, "fseek(), file too short?");
+			warn(errno, "fseek()");
 			break;
 		}
 		v1tag = malloc(TAG_SIZE);
-		if (v1tag == NULL) break;
+		if (v1tag == NULL) {
+			warn(errno, "malloc()");
+			break;
+		}
 		size_t read = fread(v1tag, 1, TAG_SIZE, fs);
 		if (read < TAG_SIZE) {
-			if (ferror(fs))
+			if (feof(fs) || ferror(fs))
 				warn(errno, "fread()");
-			else if (feof(fs))
-				warn(0, "file too short?");
 			else
 				assert(0);
 			break;
@@ -160,111 +168,62 @@ has_t has_id3v1(FILE *fs)
 			break;
 		ret = yes;
 	} while (0);
-	
+
 	if (v1tag != NULL)
 		free(v1tag);
-		
+
 	return ret;
 }
 
 int main(int argc, char *argv[])
 {
-	debug("system page size is %ld\n", sysconf(_SC_PAGESIZE));
-	
+	//debug(0, "system page size is %ld\n", sysconf(_SC_PAGESIZE));
+	debug(0, argv[1]);
 	int ret = EXIT_FAILURE;
-	/*
-	unsigned char md[20];
-	
-	if (!sha1_file(argv[1], md)) {
-		for (int i = 0; i < 20; i++) printf("%02x", md[i]);	
-		putchar('\n');
-	}
-	*/
-	
 	FILE *fs = NULL;
-	
+
 	do {
 		fs = fopen(argv[1], "r");
-		if (fs == NULL) {
+		if (!fs) {
 			warn(errno, "fopen()");
 			break;
 		}
-		__off64_t blah;
-		if (has_id3v2(fs, &blah)) {
-			assert(sizeof(blah) == sizeof(long long int));
-			debug("id3v2 flag with length %lld\n", blah);
+
+		off_t start = 0;
+		if (get_id3v2(fs, &start)) {
+			start += 10;
+			assert(sizeof(start) == sizeof(long long int));
+			debug(0, "id3v2 header with length %lld", start);
 		}
+
+		if (fseek(fs, 0, SEEK_END)) {
+			warn(errno, "fseeko()");
+			break;
+		}
+		off_t end = ftell(fs);
+		if (get_id3v1(fs)) {
+			debug(0, "id3v1 header found");
+			end -= 128;
+		}
+
+		unsigned char md[20];
+		if (sha1_file_ex(fs, md, start, end)) {
+			warn(0, "sha1_file_ex() failed");
+			break;
+		}
+
+		for (int i = 0; i < 20; i++) printf("%02x", md[i]);
+		putchar('\n');
 		ret = EXIT_SUCCESS;
 	} while (0);
 
-	if (fs != NULL) {
+	if (fs) {
 		int n = fclose(fs);
-		if (n != 0) {
+		if (n) {
 			assert(n == EOF);
 			fatal(errno, "fclose()");
 		}
 	}
-	
+
 	return ret;
 }
-
-/*
-void mp3_sig(const char *filename, char sig[])
-{
-	int ret = 0;
-	
-	FILE *fstream = fopen(filename, "r");
-	if (fstream == NULL) {
-		warn(errno, "fopen(\"%s\", \"r\")", filename);
-		ret = 1;
-	} else {
-		long bufsize = sysconf(_SC_PAGESIZE);
-		assert(sizeof(size_t) >= sizeof(long));
-		void *filedata = malloc(bufsize);
-		if (filedata == NULL) {
-			warn(errno, "malloc()");
-			ret = 1;
-		} else {
-			trm_t o = trm_New();
-			for (;;) {
-				size_t bytesread = fread(filedata, 1, bufsize, fstream);
-				if (bytesread < bufsize && ferror(fstream)) {
-					warn(errno, "fread()");
-					break;
-				}		
-				if (trm_GenerateSignature(o, filedata, bytesread) == 1) {
-					trm_FinalizeSignature(o, sig, NULL);
-					break;
-				}
-				if (bytesread < bufsize && feof(fstream)) {
-					if (trm_)
-						ret = 1;
-					break;
-				}
-			}
-			free(filedata);
-		}
-		int n = fclose(fstream);
-		if (n != 0) {
-			assert(n == EOF);
-			fatal(errno, "fclose()");
-		}
-	}
-	return ret;
-}
-*/
-
-/*
-void mp3_data_hash(const char *filename)
-{
-	struct id3_file *id3file = id3_file_open(filename, ID3_FILE_MODE_READONLY);
-	union id3_field field;
-	id3_length_t *len;
-	id3_byte_t *bindata = id3_field_getbinarydata(&field, len);
-	assert(sizeof(id3_byte_t) == sizeof(unsigned char));
-	debug("bindata == %hhd\n", bindata);
-	debug("field == %x\n", field);
-	debug("len == %u\n", len);
-	id3_file_close(id3file);
-}
-*/
