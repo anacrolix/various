fd76a2f980becd400d16212a383ff28f9802781e
matt <matt@localhost>
2008-03-25 13:51:22 +0000

    cube.c now holds the definition for EDGE_COMPASS

--- erubix/trunk/cube.c
+++ erubix/trunk/cube.c
@@ -1,6 +1,16 @@
 #include <stdlib.h>
 #include "cube.h"
 
+const enum Face_e
+EDGE_COMPASS[FACES_PER_CUBE][EDGES_PER_FACE] = {
+	{FACE_U, FACE_R, FACE_D, FACE_L}, //F
+	{FACE_B, FACE_R, FACE_F, FACE_L}, //U
+	{FACE_U, FACE_B, FACE_D, FACE_F}, //R
+	{FACE_F, FACE_R, FACE_B, FACE_L}, //D
+	{FACE_U, FACE_L, FACE_D, FACE_R}, //B
+	{FACE_U, FACE_F, FACE_D, FACE_B}  //L
+};
+
 const enum Tile_e
 EDGE_MAPPING[FACES_PER_CUBE][EDGES_PER_FACE][TILES_PER_EDGE] =
 {
@@ -34,6 +44,8 @@ EDGE_MAPPING[FACES_PER_CUBE][EDGES_PER_FACE][TILES_PER_EDGE] =
 	{TILE_TL, TILE_L, TILE_BL},
 	{TILE_BR, TILE_R, TILE_TR}}
 };
+
+
 
 void Cube_Init(Cube *cube)
 {
