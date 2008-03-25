be33b5c6ca47bc4c5f978d229d9f24dd5477ae67
matt <matt@localhost>
2008-03-25 13:50:22 +0000

    cube.c now holds the definition of EDGE_MAPPING

--- erubix/trunk/cube.c
+++ erubix/trunk/cube.c
@@ -1,6 +1,40 @@
 #include <stdlib.h>
 #include "cube.h"
 
+const enum Tile_e
+EDGE_MAPPING[FACES_PER_CUBE][EDGES_PER_FACE][TILES_PER_EDGE] =
+{
+	{{TILE_BL, TILE_B, TILE_BR},
+	{TILE_TL, TILE_L, TILE_BL},
+	{TILE_TR, TILE_T, TILE_TL},
+	{TILE_BR, TILE_R, TILE_TR}},
+
+	{{TILE_TR, TILE_T, TILE_TL},
+	{TILE_TR, TILE_T, TILE_TL},
+	{TILE_TR, TILE_T, TILE_TL},
+	{TILE_TR, TILE_T, TILE_TL}},
+
+	{{TILE_BR, TILE_R, TILE_TR},
+	{TILE_TL, TILE_L, TILE_BL},
+	{TILE_BR, TILE_R, TILE_TR},
+	{TILE_BR, TILE_R, TILE_TR}},
+
+	{{TILE_BL, TILE_B, TILE_BR},
+	{TILE_BL, TILE_B, TILE_BR},
+	{TILE_BL, TILE_B, TILE_BR},
+	{TILE_BL, TILE_B, TILE_BR}},
+
+	{{TILE_TR, TILE_T, TILE_TL},
+	{TILE_TL, TILE_L, TILE_BL},
+	{TILE_BL, TILE_B, TILE_BR},
+	{TILE_BR, TILE_R, TILE_TR}},
+
+	{{TILE_TL, TILE_L, TILE_BL},
+	{TILE_TL, TILE_L, TILE_BL},
+	{TILE_TL, TILE_L, TILE_BL},
+	{TILE_BR, TILE_R, TILE_TR}}
+};
+
 void Cube_Init(Cube *cube)
 {
 	cube->parent = NULL;
