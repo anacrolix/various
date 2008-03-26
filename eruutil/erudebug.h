3c5db036122fcc3b694ff85afbc7725e73d7f353
matt <matt@localhost>
2008-03-26 13:33:54 +0000

    restructured macros around a single #ifdef

--- eruutil/erudebug.h
+++ eruutil/erudebug.h
@@ -4,34 +4,31 @@
 #define fatal(errval, fmt, ...) \
 	(error_at_line(EXIT_FAILURE, errval, __FILE__, __LINE__, fmt, ##__VA_ARGS__))
 
-#ifdef NDEBUG
-#define warn(errval, fmt, ...) \
-	({fprintf(stderr, fmt, ##__VA_ARGS__); fputc('\n', stderr);})
-#else
-#define warn(errval, fmt, ...) \
-	(error_at_line(0, errval, __FILE__, __LINE__, fmt, ##__VA_ARGS__))
-#endif
-
-#ifdef NDEBUG
-#define debug(fmt, ...)
-#define debugln(fmt, ...)
-#else
-#define debug(fmt, ...) \
-	(fprintf(stderr, fmt, ##__VA_ARGS__))
-#define debugln(fmt, ...) \
-	({fprintf(stderr, fmt, ##__VA_ARGS__); fputc('\n', stderr);})
-#endif
-
+// should this be debug only?
 #define static_assert(cond) \
 	extern char dummy_assert_array[(cond)?1:-1]
 
 #ifdef NDEBUG
-#define debug_size(type)
+	#define verify(f) (f)
+	#define warn(errval, fmt, ...) \
+		({fprintf(stderr, fmt, ##__VA_ARGS__); fputc('\n', stderr);})
+	#define debug(fmt, ...)
+	#define debugln(fmt, ...)
+	#define debug_size(type)
 #else
-#define debug_size(type) (error_at_line(0, 0, __FILE__, __LINE__, \
-	"sizeof(" #type ") = %d", sizeof(type)))
+	#define verify(f) (assert(f))
+	#define warn(errval, fmt, ...) \
+		(error_at_line(0, errval, __FILE__, __LINE__, fmt, ##__VA_ARGS__))
+	#define debug(fmt, ...) \
+		(fprintf(stderr, fmt, ##__VA_ARGS__))
+	#define debugln(fmt, ...) \
+		({fprintf(stderr, fmt, ##__VA_ARGS__); fputc('\n', stderr);})
+	// maybe change this to psize()?
+	#define debug_size(type) (error_at_line(0, 0, __FILE__, __LINE__, \
+		"sizeof(" #type ") = %d", sizeof(type)))
 #endif
 
+// future macro? dump variable value to stderr
 /*
 #ifdef NDEBUG
 #define dump(var, fmt)
