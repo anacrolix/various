f4818f368ff97b8589e99d85640404d0e99b3928
matt <matt@localhost>
2008-03-26 13:35:24 +0000

    extern'ed Move_str into cube.c added header dep on erudebug.h to Makefile restructed cube.h

--- erubix/trunk/cube.h
+++ erubix/trunk/cube.h
@@ -1,70 +1,35 @@
 #ifndef CUBE_H
 #define CUBE_H
 
-//#include "eruutil/erudebug.h"
-
-// MOVES
-
 #define UNIQUE_MOVES 12
-enum Move_e {
-	MOVE_FCW,
-	MOVE_FACW,
-	MOVE_UCW,
-	MOVE_UACW,
-	MOVE_RCW,
-	MOVE_RACW,
-	MOVE_DCW,
-	MOVE_DACW,
-	MOVE_BCW,
-	MOVE_BACW,
-	MOVE_LCW,
-	MOVE_LACW
-};
-
-static const char *Move_str[] = {"F", "F'", "U", "U'", "R", "R'", "D", "D'", "B", "B'", "L", "L'"};
-
-// TILES
-
 #define TILES_PER_FACE 9
-/* 0 1 2
- * 3 4 5
- * 6 7 8
- */
-enum Tile_e {
-	TILE_TL,
-	TILE_T,
-	TILE_TR,
-	TILE_L,
-	TILE_C,
-	TILE_R,
-	TILE_BL,
-	TILE_B,
-	TILE_BR
-};
-
-// FACES
-
 #define FACES_PER_CUBE 6
-enum Face_e {
-	FACE_F,
-	FACE_U,
-	FACE_R,
-	FACE_D,
-	FACE_B,
-	FACE_L
+#define EDGES_PER_FACE 4
+#define TILES_PER_EDGE 3
+
+enum Move_e {
+	MOVE_FCW, MOVE_FACW,
+	MOVE_UCW, MOVE_UACW,
+	MOVE_RCW, MOVE_RACW,
+	MOVE_DCW, MOVE_DACW,
+	MOVE_BCW, MOVE_BACW,
+	MOVE_LCW, MOVE_LACW
 };
-struct Face_t {
-	char tile[TILES_PER_FACE];
+
+enum Tile_e {
+		/* 0 1 2
+		 * 3 4 5
+		 * 6 7 8 */
+	TILE_TL, TILE_T, TILE_TR,
+	TILE_L, TILE_C, TILE_R,
+	TILE_BL, TILE_B, TILE_BR
 };
+
+enum Face_e {FACE_F, FACE_U, FACE_R, FACE_D, FACE_B, FACE_L};
+struct Face_t {char tile[TILES_PER_FACE];};
 typedef struct Face_t Face;
-static_assert(sizeof(Face) == 9);
 
-enum Rotation_e {
-	ROTATE_ACW,
-	ROTATE_CW
-};
-
-// CUBE
+enum Rotation_e {ROTATE_ACW, ROTATE_CW};
 
 typedef struct Cube_t {
 	struct Cube_t *parent;
@@ -74,15 +39,14 @@ typedef struct Cube_t {
 	struct Cube_t *nextMove[UNIQUE_MOVES];
 } Cube;
 
-static_assert(sizeof(Cube) == 116);
-
-#define EDGES_PER_FACE 4
+extern const char *Move_str[];
 extern const enum Face_e
 EDGE_COMPASS[FACES_PER_CUBE][EDGES_PER_FACE];
-
-#define TILES_PER_EDGE 3
 extern const enum Tile_e
 EDGE_MAPPING[FACES_PER_CUBE][EDGES_PER_FACE][TILES_PER_EDGE];
+
+static_assert(sizeof(Face) == 9);
+static_assert(sizeof(Cube) == 116);
 
 Cube *Cube_New();
 void Cube_Init(Cube *);
