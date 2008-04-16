3a36085c0496a07aace64376a8e3c6a0cd3024c3
matt <matt@localhost>
2008-04-16 08:22:14 +0000

    cleaned up Makefile allowed raw and default input of simulation parameters added negative max steps as infinite simulation

--- pymd/md.py
+++ pymd/md.py
@@ -3,14 +3,22 @@
 from visual import sphere, vector, rate
 from random import random
 import md
+from time import clock
 
 radius = 0.25
-max_steps = 1
-max_substeps = 9
-dt = 0.001
 
-side_len = float(raw_input("Enter cube side length: "))
-mols_per_side = int(raw_input("Enter molecules to a side: "))
+print "Simulation parameters (enter for default)"
+try: side_len = float(raw_input("Cube side length (4): "))
+except ValueError: side_len = 4.0
+try: mols_per_side = int(raw_input("Molecules to a side (4): "))
+except ValueError: mols_per_side = 4
+try: max_substeps = int(raw_input("Steps per frame (10): "))
+except ValueError: max_substeps = 10
+try: max_steps = int(raw_input("Total steps (-1):"))
+except ValueError: max_steps = -1
+try: dt = float(raw_input("Time step (0.001): "))
+except ValueError: dt = 0.001
+
 molecules = []
 
 for x in range(mols_per_side):
@@ -35,9 +43,12 @@ def print_energy_data(molecules):
 print "%15s%15s%15s%15s" % ("time", "total", "pe", "ke")
 
 step = 0
-while step < max_steps:
-	rate(60)
+start_time = clock()
+while 1:
 	print_energy_data(molecules)
 	step += md.update_mols(molecules, max_substeps, dt)
+	if step < max_steps and max_steps >= 0: break
+end_time = clock()
 
 print_energy_data(molecules)
+print "Performed", step, "steps in", end_time - start_time, "s"
