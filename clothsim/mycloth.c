e42adc39836fbdab1688381dc1bfbae5b025e9b9
matt <matt@localhost>
2008-05-29 12:37:09 +0000

    zomg crap plx

--- clothsim/mycloth.c
+++ clothsim/mycloth.c
@@ -1,4 +1,5 @@
 #include "cloth.h"
+#include <omp.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
@@ -8,7 +9,7 @@
 
 #define VEL_DAMP 0.995
 
-typedef long unsigned index_t;
+typedef long index_t;
 
 struct vector {	double x, y, z; };
 #define vector_new(x, y, z, i) ((struct vector){x[(i)], y[(i)], z[(i)]})
@@ -22,6 +23,7 @@ struct vector {	double x, y, z; };
 double *fx, *fy, *fz; // forces
 double *vx, *vy, *vz; // velocities
 double *oldfx, *oldfy, *oldfz; // old forces
+//double *intsqrts; // precalculated integer square roots
 
 // useful macros
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
@@ -29,7 +31,8 @@ double *oldfx, *oldfy, *oldfz; // old forces
 #define for_each_cloth_node(a, b) \
 	for (index_t a = 0; a < b*b; a++)
 #define mag(x, y, z) (sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)))
-
+#define SWAP(a, b) do { \
+	__typeof__((a)) _c = (a); a = b; b = _c; } while (false)
 /* VECTOR STUFF */
 
 #define vector_mag(v) (mag((v).x, (v).y, (v).z))
@@ -39,40 +42,67 @@ struct vector vector_scale(struct vector v, double n)
 	return (struct vector){v.x * n, v.y * n, v.z * n};
 }
 
+inline void __eval_pef(int n, double sep,
+	double fcon, double *x, double *y, double *z, double *fx,
+	double *fy, double *fz, int num_threads, struct vector *f,
+	index_t t, index_t dx, index_t dy, index_t i, index_t j)
+{
+	//assert(i != dx || j != dy);
+	//double len = intsqrts[((i - dx) * (i - dx)) + ((j - dy) * (j - dy))] * sep;
+	double len = sqrt((double)((i - dx) * (i - dx) + (j - dy) * (j - dy)) * sep);
+	double r12_x = x[dx * n + dy] - x[t];
+	double r12_y = y[dx * n + dy] - y[t];
+	double r12_z = z[dx * n + dy] - z[t];
+	double r12_mag = mag(r12_x, r12_y, r12_z);
+	pe += fcon * pow(r12_mag * len, 2);
+	double a = (r12_mag - len) * fcon / len;
+	f->x += a * r12_x;
+	f->y += a * r12_y;
+	f->z += a * r12_z;
+}
+
 // calculates PE and F
 double eval_pef(int n, int delta, double grav, double sep,
-                double fcon, double *x, double *y, double *z,		
-                double *fx, double *fy, double *fz)
+                double fcon, double *x, double *y, double *z,
+                double *fx, double *fy, double *fz, int num_threads)
 {
-  //loop over particles
-      //loop over displacements
-	  // exclude self interaction
-	    // compute reference distance
-	    // compute actual distance
-	    //potential energy and force
 	pe = 0.0;
-	for (long i = 0; i < n; i++) {
-		for (long j = 0; j < n; j++) {
-			long t = i * n + j;
-			fx[t] = 0.0;
-			fy[t] = 0.0;
-			fz[t] = -grav;
-			for (long dx = MAX(i - delta, 0); dx < MIN(i + delta + 1, n); dx++) {
-				for (long dy = MAX(j - delta, 0); dy < MIN(j + delta + 1, n); dy++) {
-					double len = sqrt((pow(i - dx, 2) + pow(j - dy, 2)) * sep);
-					//double len = 1.0;
-					if (i != dx || j != dy) {
-						double r12_x = x[dx * n + dy] - x[i * n + j];
-						double r12_y = y[dx * n + dy] - y[i * n + j];
-						double r12_z = z[dx * n + dy] - z[i * n + j];
-						double r12_mag = mag(r12_x, r12_y, r12_z);
-						pe += fcon * pow(r12_mag * len, 2);
-						fx[i * n + j] += fcon * r12_x / len * (r12_mag - len);
-						fy[i * n + j] += fcon * r12_y / len * (r12_mag - len);
-						fz[i * n + j] += fcon * r12_z / len * (r12_mag - len);
+	#pragma omp parallel for
+	for (index_t i = 0; i < n; i++) {
+		#pragma omp parallel for
+		for (index_t j = 0; j < n; j++) {
+			index_t t = i * n + j;
+			struct vector f = {0.0, 0.0, -grav};
+			for (index_t dx = MAX(i - delta, 0); dx < MIN(i + delta + 1, n); dx++)
+			{
+				if (j - delta >= 0 && j + delta < n) {
+					for (index_t dy = j - delta; dy < j; dy++)
+					{
+						__eval_pef(n, sep, fcon, x, y, z,
+							fx, fy, fz, num_threads, &f, t, dx, dy, i,
+							j);
+					}
+					if (i != dx) __eval_pef(n, sep, fcon, x, y, z, fx, fy, fz, num_threads, &f, t, dx, j, i, j);
+					for (index_t dy = j + 1; dy < j + delta + 1; dy++)
+					{
+						__eval_pef(n, sep, fcon, x, y, z,
+							fx, fy, fz, num_threads, &f, t, dx, dy, i,
+							j);
+					}
+				} else {
+					for (index_t dy = MAX(j - delta, 0); dy < MIN(j + delta + 1, n); dy++)
+					{
+						if (i != dx || j != dy) {
+							__eval_pef(n, sep, fcon, x, y, z,
+								fx, fy, fz, num_threads, &f, t, dx, dy, i,
+								j);
+						}
 					}
 				}
 			}
+			fx[t] = f.x;
+			fy[t] = f.y;
+			fz[t] = f.z;
 		}
 	}
 	return pe;
@@ -80,12 +110,12 @@ double eval_pef(int n, int delta, double grav, double sep,
 
 // allocate memory and initialize PE and F
 void initialize(int n, float mass, float fcon, int delta, float grav,
-	float sep, float ballsize, float dt,
-	double *x, double *y, double *z)
+	float sep, float ballsize, float dt, double *x, double *y, 
+	double *z, int num_threads)
 {
 	// allocate and zero F and v arrays
-	double **simarrs[] = {&fx, &fy, &fz,
-		&vx, &vy, &vz, &oldfx, &oldfy, &oldfz};
+	double **simarrs[] =
+		{&fx, &fy, &fz, &vx, &vy, &vz, &oldfx, &oldfy, &oldfz};
 	index_t const simarrs_len = 
 		sizeof(simarrs) / sizeof(__typeof__(*simarrs));
 	assert(simarrs_len == 9);
@@ -95,26 +125,37 @@ void initialize(int n, float mass, float fcon, int delta, float grav,
 			(*simarrs[i])[j] = 0.0;
 		}
 	}
+	/*
+	// precalculate integer square roots
+	intsqrts = malloc(sizeof(double) * delta * delta * 2 + 1);
+	for (index_t i = 0; i < delta * delta * 2 + 1; i++)
+		intsqrts[i] = sqrt(i);
+	*/
+	// set maximum omp threads
+	//omp_set_num_threads(num_threads);
 	// evaluate potential and force
-	pe = eval_pef(n, delta, grav, sep, fcon, x, y, z, fx, fy, fz);
+	pe = eval_pef(n, delta, grav, sep, fcon, x, y, z, fx, fy, fz,
+		num_threads);
 }
 
 // this is the the body for one integration timestep
 void loopcode(int n, float mass, float fcon,
 	      int delta, float grav, float sep, float ballsize,
-	      float dt, double *x, double *y, double *z)
+	      float dt, double *x, double *y, double *z, int num_threads)
 {
-	//update position as per MD simulation, copy force array
+	// update position as per MD simulation, copy force array
+	#pragma omp parallel for schedule(guided, n)
 	for (index_t i = 0; i < n*n; i++) {
 		x[i] += dt * (vx[i] + dt * fx[i] / 2.0);
 		y[i] += dt * (vy[i] + dt * fy[i] / 2.0);
 		z[i] += dt * (vz[i] + dt * fz[i] / 2.0);
 	}
-	memcpy(oldfx, fx, sizeof(*fx) * n * n);
-	memcpy(oldfy, fy, sizeof(*fy) * n * n);
-	memcpy(oldfz, fz, sizeof(*fz) * n * n);
+	SWAP(fx, oldfx);
+	SWAP(fy, oldfy);
+	SWAP(fz, oldfz);
 
 	// push node to outside of cloth, remove radial velocity
+	#pragma omp parallel for schedule(guided, n)
 	for_each_cloth_node(i, n) {
 		struct vector sep = vector_new(x, y, z, i);
 		double dist = vector_mag(sep);
@@ -132,13 +173,15 @@ void loopcode(int n, float mass, float fcon,
 	}
 
 	// calculate force and PE at new coordinates
-	pe = eval_pef(n, delta, grav, sep, fcon, x, y, z, fx, fy, fz);
+	pe = eval_pef(n, delta, grav, sep, fcon, x, y, z, fx, fy, fz,
+		num_threads);
 	
 	// dampen velocity
+	#pragma omp parallel for
 	for_each_cloth_node(i, n) {
 		vector_out(vector_scale(vector_new(vx, vy, vz, i), VEL_DAMP), vx, vy, vz, i);
 	}
-		
+	#pragma omp parallel for
 	for (index_t i = 0; i < n*n; i++) {
 		vx[i] += dt * (fx[i] + oldfx[i]) / 2.0;
 		vy[i] += dt * (fy[i] + oldfy[i]) / 2.0;
