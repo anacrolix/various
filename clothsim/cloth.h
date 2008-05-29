e325cd5d3e764296676f25e2c59710b672200b19
matt <matt@localhost>
2008-05-29 14:25:10 +0000

    sux0rs!!!

--- clothsim/cloth.h
+++ clothsim/cloth.h
@@ -1,9 +1,5 @@
-void initialize(int n, float mass, float fcon, int delta, float grav,
-	float sep, float ballsize, float dt, double *x, double *y,
-	double *z, int num_threads);
-
-void loopcode(int n, float mass, float fcon,
-	int delta, float grav, float sep, float ballsize,
-	float dt, double *x, double *y, double *z, int num_threads);
+void initialize();
+void loopcode();
 
 double pe;
+double *x, *y, *z;
