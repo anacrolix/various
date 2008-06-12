79a9660257903f707837a94e7cce5a9c33757ca6
matt <matt@localhost>
2008-06-12 15:07:20 +0000

    handles data in incomplete chunks does not require the first 2 states in entirety can complete all 3 states in less than 3 read events reads data a single byte at a time for debugging does not assert when clobbering existing files

--- pygtkdndp2p/ftserv.py
+++ pygtkdndp2p/ftserv.py
@@ -8,12 +8,14 @@ class ReceiveTest(asyncore.dispatcher):
 	in_buffer = str()
 	def process_data(self):
 		if self.path == None:
-			term_pos = self.in_buffer.index('\r\n')
+			try: term_pos = self.in_buffer.index('\r\n')
+			except ValueError: return
 			self.path = self.in_buffer[:term_pos]
 			print "Path is", self.path
 			self.in_buffer = self.in_buffer[term_pos+2:]
 		elif self.file_length == None:
-			term_pos = self.in_buffer.index('\r\n')
+			try: term_pos = self.in_buffer.index('\r\n')
+			except ValueError: return
 			self.file_length = int(self.in_buffer[:term_pos])
 			print "File length is", self.file_length
 			self.in_buffer = self.in_buffer[term_pos+2:]
@@ -21,15 +23,18 @@ class ReceiveTest(asyncore.dispatcher):
 			print "File \"", self.path, "\"received."
 			print len(self.in_buffer)
 			assert(len(self.in_buffer) == self.file_length)
-			assert(not os.path.exists(self.path))
+			#assert(not os.path.exists(self.path))
 			open(self.path, 'wb').write(self.in_buffer)
 			self.close()
+			return
+		if (self.path == None or self.file_length == None) and len(self.in_buffer) > 0:
+			self.process_data()
 	def writable(self):
 		return False
 	def handle_close(self):
 		self.close()
 	def handle_read(self):
-		self.in_buffer += self.recv(0x1000)
+		self.in_buffer += self.recv(1)
 		self.process_data()
 
 class ListenTest(asyncore.dispatcher):
