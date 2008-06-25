9fbc8c33add417940de9d2abd2436d58f19544ed
matt <matt@localhost>
2008-06-25 14:25:10 +0000

    fleshing out client prog

--- erup2p/client.py
+++ erup2p/client.py
@@ -5,6 +5,7 @@ import threading
 import asyncore, asynchat
 import socket
 import os
+import time
 
 class ServerHandler():
 	
@@ -18,11 +19,27 @@ class ServerHandler():
 	
 	def __init__(self, notify_cb):
 		
-		self.dispatcher = ServerDispatcher()		
+		self.dispatcher = ServerHandler.ServerDispatcher()		
 
 class AsyncSockThread(threading.Thread):
+
+	quit = False
 	
-	pass
+	def __init__(self, timeout=1.0):
+		
+		threading.Thread.__init__(self)
+		self.timeout = timeout
+	
+	def run(self):
+		
+		while True:
+			asyncore.loop(timeout=self.timeout)
+			if self.quit: break
+			time.sleep(self.timeout)
+	
+	def stop(self):
+		
+		self.quit = True
 
 class PeerFileDropTarget(wx.FileDropTarget):
 	
@@ -37,6 +54,33 @@ class MainFrame(wx.Frame):
 	def __init__(self):
 		
 		wx.Frame.__init__(self, None, title='Eru P2P')
+		
+		# mb = menubar, m = menu, mi = menuitem
+		mb = wx.MenuBar()
+		m = wx.Menu()
+		mi = m.Append(-1, text='&Connect')
+		self.Bind(wx.EVT_MENU, self.on_connect, mi)
+		m.AppendSeparator()
+		mi = m.Append(-1, text='&Quit')
+		self.Bind(wx.EVT_MENU, self.on_quit, mi)
+		mb.Append(m, '&Server')
+		m = wx.Menu()
+		mi = m.Append(-1, '&About')
+		self.Bind(wx.EVT_MENU, self.on_about, mi)
+		mb.Append(m, '&Help')
+		self.SetMenuBar(mb)
+	
+	def on_connect(self, event):
+		
+		print "MainFrame:on_connect"
+		
+	def on_about(self, event):
+		
+		print "MainFrame:on_about"
+		
+	def on_quit(self, event):
+		
+		self.Destroy()
 	
 class ClientApp(wx.App):
 	
@@ -48,7 +92,22 @@ class ClientApp(wx.App):
 		self.SetTopWindow(self.main_frame)
 		self.main_frame.Show()
 		
+		self.sock_thread = AsyncSockThread()
+		self.sock_thread.start()
+		
+		self.server_handler = ServerHandler(self.notify_cb)
+		
 		return True
+		
+	def OnExit(self):
+		
+		print "Closing sockets..."
+		self.sock_thread.stop()
+		self.sock_thread.join()
+		
+	def notify_cb(self, attrfunc, *args):
+		
+		wx.CallAfter(self.__getattribute__(attrfunc), *args)
 
 def main():
 	
