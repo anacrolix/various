df0b8df6b0ebff59c406f4b62e00f3afdfe06d7d
matt <matt@localhost>
2008-06-26 15:34:30 +0000

    globalised LINE_TERM began standardizing callback system replaced object.__getattribute__(attr) with getattr(object, attr) moved a lot of stub handling to higher levels in the class hierarchy added a listbox to MainFrame modified protocol to use eval() and repr() for packing cleaned up the server to use async_chat

--- erup2p/client.py
+++ erup2p/client.py
@@ -7,19 +7,64 @@ import socket
 import os
 import time
 
+LINE_TERM = '\r\n'
+
 class ServerHandler():
 	
 	class ServerDispatcher(asynchat.async_chat):
 		
-		pass
+		def notify(self, event, *args):
+			
+			self.notify_cb(self, event, *args)
 		
-	def notify(self, event, *args):
+		def __init__(self, notify_cb):
+			
+			asynchat.async_chat.__init__(self)
+			self.notify_cb = notify_cb
+			self.buffer = ''
+			self.set_terminator(LINE_TERM)
+			
+		def collect_incoming_data(self, data):
+			
+			self.buffer += data
 		
-		self.notify_cb(event, *args)
+		def found_terminator(self):
+			
+			message = eval(self.buffer)
+			self.buffer = ''
+			assert len(message) == 2
+			self.notify(message[0], *message[1])
+						
+		def handle_connect(self):
+			
+			self.notify("connected")
+	
+	def send(self, header, *data):
+		
+		self.dispatcher.send(repr((header, data)) + LINE_TERM)
 	
 	def __init__(self, notify_cb):
 		
-		self.dispatcher = ServerHandler.ServerDispatcher()		
+		self.notify_cb = notify_cb
+		#dispatcher = None
+
+	def handle_dispatcher(self, caller, event, *args):
+		
+		assert caller == self.dispatcher
+		self.notify_cb(event, *args)
+
+	def connect(self, address):
+		
+		try: self.dispatcher.close()
+		except AttributeError: pass
+		self.dispatcher = self.ServerDispatcher(self.handle_dispatcher)
+		self.dispatcher.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.dispatcher.connect(address)
+	
+	def close(self):
+		
+		if self.dispatcher:
+			self.dispatcher.close()
 
 class AsyncSockThread(threading.Thread):
 
@@ -34,6 +79,7 @@ class AsyncSockThread(threading.Thread):
 		
 		while True:
 			asyncore.loop(timeout=self.timeout)
+			print "asyncore: No sockets"
 			if self.quit: break
 			time.sleep(self.timeout)
 	
@@ -51,9 +97,13 @@ class PeerFrame(wx.Frame):
 
 class MainFrame(wx.Frame):
 	
-	def __init__(self):
+	def __init__(self, notify_cb):
 		
 		wx.Frame.__init__(self, None, title='Eru P2P')
+		self.notify_cb = notify_cb
+		
+		lb = wx.ListBox(self)
+		lb.Append('wank stick')
 		
 		# mb = menubar, m = menu, mi = menuitem
 		mb = wx.MenuBar()
@@ -73,6 +123,7 @@ class MainFrame(wx.Frame):
 	def on_connect(self, event):
 		
 		print "MainFrame:on_connect"
+		self.notify_cb("connect")
 		
 	def on_about(self, event):
 		
@@ -88,14 +139,14 @@ class ClientApp(wx.App):
 		
 		self.peer_frames = {}
 		
-		self.main_frame = MainFrame()
+		self.main_frame = MainFrame(self.handle_mainframe)
 		self.SetTopWindow(self.main_frame)
-		self.main_frame.Show()
+		self.main_frame.Show()		
 		
 		self.sock_thread = AsyncSockThread()
 		self.sock_thread.start()
 		
-		self.server_handler = ServerHandler(self.notify_cb)
+		self.server_handler = ServerHandler(self.handle_server)
 		
 		return True
 		
@@ -103,12 +154,28 @@ class ClientApp(wx.App):
 		
 		print "Closing sockets..."
 		self.sock_thread.stop()
+		self.server_handler.close()
 		self.sock_thread.join()
 		
-	def notify_cb(self, attrfunc, *args):
+	def handle_server(self, event, *data):
 		
-		wx.CallAfter(self.__getattribute__(attrfunc), *args)
-
+		#wx.CallAfter(getattr(self, attrfunc), *args)
+		getattr(self, 'server_' + event)(*data)
+		
+	def handle_mainframe(self, event, *args):
+		
+		assert event == "connect" and len(args) == 0
+		self.server_handler.connect(('localhost', 3000))
+		
+	def server_connected(self):
+		
+		self.server_handler.send('login')
+		self.server_handler.send('setname', 'penis_' + str(os.getpid()))
+		
+	def server_login(self, peer):
+		
+		pass
+	
 def main():
 	
 	app = ClientApp()
