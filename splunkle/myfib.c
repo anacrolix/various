e705aad15fd68a0ff93de3ac1ca1c163a0499d12
david <david@localhost>
2008-08-20 15:47:01 +0000

    added recursive fibonacci

--- splunkle/myfib.c
+++ splunkle/myfib.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-intmax_t fib(intmax_t, intmax_t, intmax_t);
+intmax_t fib(int, intmax_t, intmax_t);
 
 int main(int argc, char *argv[])
 {
-	intmax_t n, value = atoi(argv[1]);
+	const char *usage = "Takes one argument, n.  Outputs the nth number in the fibb sequence";
+	intmax_t value;
+	int n = atoi(argv[1]);
 	if (n<0) {
 		printf(usage);
 		exit(EXIT_FAILURE);
@@ -21,12 +23,13 @@ int main(int argc, char *argv[])
 	return EXIT_SUCCESS;
 }
 
-intmax_t fib(intmax_t n, intmax_t x, intmax_t y)
+intmax_t fib(int n, intmax_t x, intmax_t y)
 {
 	if (n == 1) {
 		return y;
 	} else {
 		return fib(n-1, y, x+y);
+	}
 }
 
 /*In Haskell, this is easier:
