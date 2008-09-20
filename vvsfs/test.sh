6a151448f2a9b7a91c5e71808e163b075b7043b5
matt <matt@localhost>
2008-09-20 12:34:37 +0000

    added a simple bash testing script

---
+++ vvsfs/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mntdir="testmountpoint"
+loopdev="testvvsfs.img"
+
+cleanup () {
+	sudo umount $mntdir
+	sudo rmmod vvsfs
+	rm -rvf $mntdir
+}
+
+make all
+if [ "$1" == "down" ]; then
+	cleanup
+	exit 0
+fi
+dd if=/dev/zero of="$loopdev" bs=512 count=100
+./mkfs.vvsfs "$loopdev"
+mkdir "$mntdir"
+sudo insmod vvsfs.ko
+sudo mount -o loop "$loopdev" "$mntdir"
+if [ "$1" == "up" ]; then
+	exit 0
+fi
+
+cleanup
+exit 0
