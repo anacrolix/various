dfd9fee418fc12e2408b1f407530d72207cfef6f
matt <matt@localhost>
2008-09-20 12:35:49 +0000

    formatting cleanup

--- vvsfs/vvsfs.h
+++ vvsfs/vvsfs.h
@@ -3,23 +3,21 @@
 #define NUMBLOCKS 100
 #define MAXNAME 15
 
-#define MAXFILESIZE (BLOCKSIZE - 3*sizeof(int))
+#define MAXFILESIZE (BLOCKSIZE - 3 * sizeof(int))
 
-#define MIN(a,b) (((a)<(b))?(a):(b))
-
+#define MIN(a,b) (((a) < (b)) ? (a) : (b))
 
 #define true 1
 #define false 0
 
-
 struct vvsfs_inode {
-  int is_empty;
-  int is_directory;
-  int size;
-  char data[MAXFILESIZE];
+	int is_empty;
+	int is_directory;
+	int size;
+	char data[MAXFILESIZE];
 };
 
 struct vvsfs_dir_entry {
-  char name[MAXNAME+1];
-  int inode_number;
+	char name[MAXNAME+1];
+	int inode_number;
 };
