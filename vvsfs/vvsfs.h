69a325b114305e4682af0c7ca46584f8533daa00
matt <matt@localhost>
2008-09-22 16:01:28 +0000

    mkfs.vvsfs.c: 	* update to use new vvsfs_inode format vvsfs.c: 	* vvsfs_readblock(), vvsfs_writeblock(): check for null super block 	root dentry pointer before dereference 	* added vvsfs_iget() to read from in inodes during dir lookups 	* vvsfs_lookup(): defer inode allocation and device retrieval to 	vvsfs_iget() 	* vvsfs_read_inode(): comment out contents, deprecated 	* added vvsfs_write_inode() to write inodes to device 	* vvsfs_ops: undeprecated designated initializers vvsfs.h: 	* struct vvsfs_inode: added new members {a,m,c}time, {u,g}id, mode 	and size; merged is_empty and is_directory into new member "flags"

--- vvsfs/vvsfs.h
+++ vvsfs/vvsfs.h
@@ -3,19 +3,31 @@
 #define NUMBLOCKS 100
 #define MAXNAME 15
 
-#define MAXFILESIZE (BLOCKSIZE - 3 * sizeof(int))
+#define static_assert(cond) \
+	__attribute__((unused)) extern char \
+	dummy_assert_array[(cond) ? 1 : -1]
+
+#define MAXFILESIZE 484
 
 #define MIN(a,b) (((a) < (b)) ? (a) : (b))
 
 #define true 1
 #define false 0
 
+/* vvsfs inode flags */
+#define VVSFS_IF_DIR 1
+#define VVSFS_IF_EMPTY 2
+
+//#define VVSFS_IF_
+
 struct vvsfs_inode {
-	int is_empty;
-	int is_directory;
-	int size;
+	int32_t atime, mtime, ctime;
+	uint32_t flags, uid, gid;
+	uint16_t mode, size;
 	char data[MAXFILESIZE];
 };
+
+static_assert(sizeof(struct vvsfs_inode) == 512);
 
 struct vvsfs_dir_entry {
 	char name[MAXNAME+1];
