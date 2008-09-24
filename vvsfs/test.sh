3b32ef7c29de31eedee77a4e1270539e7807ebff
matt <matt@localhost>
2008-09-24 00:38:41 +0000

    added summary docs and some simple command line tests to test.sh

--- vvsfs/test.sh
+++ vvsfs/test.sh
@@ -22,6 +22,22 @@ sudo mount -o loop "$loopdev" "$mntdir"
 if [ "$1" == "up" ]; then
 	exit 0
 fi
-
+cd "$mntdir"
+for fn in a b c d e; do
+	echo $fn > $fn
+done
+for z in 1 2 3; do
+	for fn in *; do
+		cp -v $fn "${fn}${fn}"
+	done
+done
+cat *
+for fn in * ; do
+	echo '' > $fn
+	echo lol > $fn
+done
+cat *
+rm -v *
+cd ..
 cleanup
 exit 0
