a36ec6387855d6e8a408f9302075494e3aa8feff
matt <matt@localhost>
2008-10-05 14:52:26 +0000

    * added some info about curstar * can change the volume with up and down keys in the play window * more descriptive string for pipeline transition and state * display initialization errors * dont use intro screen delay to mask race condition * ubuntu compilation requirements

--- curstar/main.c
+++ curstar/main.c
@@ -1,3 +1,21 @@
+/*
+A ncurses/gstreamer single-file audio player.
+
+Developers: Eruanno (October 2008)
+Testing: Winter, Erikina
+
+Required libs: ncurses, glibc, gnomevfs, gstreamer
+Gst-plugins: "playbin" element from gstreamer-plugins-base
+
+The initial thread handles ncurses. A spawned thread handles the the pipeline(s)
+and a glib mainloop. Stderr is directed to 'errlog' in the working directory.
+
+A single URI or local file path is passed in as a parameter and played once.
+
+After the intro screen, spacebar toggles play/pause, 'q' quits, and up and down
+arrow keys change the volume.
+*/
+
 #include <assert.h>
 #include <curses.h>
 #include <stdio.h>
@@ -8,13 +26,28 @@
 
 #define E9 1000000000LL
 
+typedef enum { UP, DOWN, QUERY } VolumeChange;
+
 struct gst_data {
 	GMainLoop *loop;
 	gchar *uri;
 	GstTagList *tags;
 	GstElement *pipe;
 	gchar *sink;
+	GError *error;
+	/* cond & mutex protect init */
+	gboolean init;
+	GCond *cond;
+	GMutex *mutex;
 };
+
+void gst_data_set_init(struct gst_data *data)
+{
+	g_mutex_lock(data->mutex);
+	data->init = TRUE;
+	g_cond_broadcast(data->cond);
+	g_mutex_unlock(data->mutex);
+}
 
 void tags_foreach(
 	GstTagList const *list, gchar const *tag, gpointer user)
@@ -31,11 +64,9 @@ gboolean bus_watch(
 	switch (GST_MESSAGE_TYPE(message)) {
 	case GST_MESSAGE_ERROR: {
 		/* print the error */
-		GError *error;
-		gst_message_parse_error(message, &error, NULL);
-		g_printerr("%s: %s\n", msgtype, error->message);
-		g_error_free(error);
-		//g_main_loop_quit(data->main_loop);
+		if (data->error) g_error_free(data->error);
+		gst_message_parse_error(message, &data->error, NULL);
+		g_printerr("%s: %s\n", msgtype, data->error->message);
 		break;
 	}
 	case GST_MESSAGE_TAG: {
@@ -59,8 +90,12 @@ gpointer audio_thread_func(gpointer _data)
 
 	/* create the playbin pipeline */
 	data->pipe = gst_element_factory_make("playbin", NULL);
-	g_return_val_if_fail(data->pipe, FALSE);
-	
+	if (!data->pipe) {
+		gst_data_set_init(data);
+		g_warn_if_reached();
+		return FALSE;
+	}
+		
 	gpointer retval = FALSE;
 	
 	/* choose and set an audio sink */
@@ -71,12 +106,14 @@ gpointer audio_thread_func(gpointer _data)
 	for (sf_name = sink_factories; *sf_name && !sink; sf_name++);
 		sink = gst_element_factory_make(*sf_name, NULL);
 	if (!sink) {
+		gst_data_set_init(data);
 		g_warn_if_reached();
 		goto fail_sink;
 	}
 	data->sink = g_strdup(*sf_name);
 	g_object_set(data->pipe, "audio-sink", sink, "video-sink", NULL, NULL);
 
+	gst_data_set_init(data); /* let the main thread proceed */
 	data->loop = g_main_loop_new(NULL, FALSE);
 
 	/* watch for pipeline events */
@@ -121,26 +158,78 @@ static void print_song_line(
 	g_free(s);
 }
 
+static char const *gst_state_to_string(GstState state)
+{
+	switch (state) {
+		case GST_STATE_NULL: return "Initializing";
+		case GST_STATE_READY: return "Ready";
+		case GST_STATE_PAUSED: return "Paused";
+		case GST_STATE_PLAYING: return "Playing";
+		default: g_return_val_if_reached(NULL);
+	}
+}
+
+
+static char const *gst_state_transition_to_string(GstStateChange change)
+{
+	switch (change) {
+		case GST_STATE_CHANGE_NULL_TO_READY: return "Initializing";
+		case GST_STATE_CHANGE_READY_TO_PAUSED: return "Loading";
+		case GST_STATE_CHANGE_PAUSED_TO_PLAYING: return "Starting";
+		case GST_STATE_CHANGE_PLAYING_TO_PAUSED: return "Pausing";
+		case GST_STATE_CHANGE_PAUSED_TO_READY: return "Stopping";
+		case GST_STATE_CHANGE_READY_TO_NULL: return "Deallocating";
+		default: g_return_val_if_reached(NULL);
+	}
+}
+
+/**
+@return: Volume as a percentage of maximum
+*/
+gdouble change_volume(GstElement *playbin, VolumeChange vc)
+{
+	gdouble volume;
+	g_object_get(playbin, "volume", &volume, NULL);
+	switch (vc) {
+	case UP:
+		volume += 0.1;
+		if (volume > 1.0) volume = 1.0;
+		break;
+	case DOWN: 
+		volume -= 0.1; 
+		if (volume < 0.0) volume = 0.0;
+		break;
+	case QUERY:
+		goto done;
+	}
+	g_object_set(playbin, "volume", volume, NULL);
+done:
+	return 100 * volume;
+}
+
 void print_song(
 	struct gst_data *data, WINDOW *win, int rows, int cols)
 {
 	/* print audio sink name */
 	mvwprintw(win, 0, 0, "audio-sink: %s", data->sink);
 	wclrtoeol(win);
+	mvwprintw(win, 1, 0, "volume: %3.0f%%", change_volume(data->pipe, QUERY));
+	wclrtoeol(win);
 
 	wmove(win, rows / 2 - 3, 0);
 
 	/* print state */
-	GstState state;
-	gst_element_get_state(
-		data->pipe, &state, NULL, GST_CLOCK_TIME_NONE);
-	char *s;
-	switch (state) {
-		case GST_STATE_PAUSED: s = "paused"; break;
-		case GST_STATE_PLAYING: s = "playing"; break;
-		default: s = NULL;
+	GstState state = GST_STATE(data->pipe);
+	GstState pending = GST_STATE_PENDING(data->pipe);
+		
+	if (pending != GST_STATE_VOID_PENDING) {
+		g_assert(state != pending);
+		print_song_line(
+			win, cols, "%s...",
+			gst_state_transition_to_string(GST_STATE_TRANSITION(state, pending)));
+	} else {
+		print_song_line(win, cols, "%s", gst_state_to_string(state));
 	}
-	print_song_line(win, cols, "%s", s);
 	
 	/* print uri */
 	char *intelligible = gnome_vfs_unescape_string_for_display(data->uri);
@@ -183,22 +272,29 @@ void main_menu(struct gst_data *data)
 
 	print_song(data, playwin, playwin_rows, playwin_cols);
 
+	assert(keypad(playwin, TRUE) == OK);
 	noecho();
 	halfdelay(1);
 	while (TRUE) {
-		int ch = getch();
+		int ch = wgetch(playwin);
 		switch (ch) {
 		case ' ': {
 			GstState state;
 			gst_element_get_state(
-				data->pipe, &state, NULL, GST_CLOCK_TIME_NONE);
+				data->pipe, &state, NULL, 0LL);
 			state = (state == GST_STATE_PAUSED) ? GST_STATE_PLAYING : GST_STATE_PAUSED;
 			gst_element_set_state(data->pipe, state);
 			break;
 		}
 		case 'q':
 			goto quit;
-		/* TODO: use up down keys to manipulate volume */
+		case KEY_UP:
+			change_volume(data->pipe, UP);
+			break;			
+		case KEY_DOWN: {
+			change_volume(data->pipe, DOWN);
+			break;
+		}
 		default:
 			break;
 		}
@@ -229,8 +325,7 @@ int main(int argc, char *argv[])
 		/* TODO: will argv[1] work if gstreamer is passed args? */
 		fprintf(stderr, "Usage: %s <URI>\n", argv[0]);
 		return EXIT_FAILURE;
-	}
-		
+	}		
 
 	/* shares variables between gst and curses threads */
 	struct gst_data data = {
@@ -238,6 +333,9 @@ int main(int argc, char *argv[])
 		.loop = NULL,
 		.pipe = NULL,
 		.tags = gst_tag_list_new(),
+		.mutex = g_mutex_new(),
+		.cond = g_cond_new(),
+		.error = NULL,
 	};
 	
 	g_printerr("%s\n", data.uri);
@@ -246,6 +344,23 @@ int main(int argc, char *argv[])
 	GThread *gst_thread = g_thread_create(
 		audio_thread_func, &data, TRUE, NULL);
 
+	/* wait for thread to initialize */
+	g_mutex_lock(data.mutex);
+	while (!data.init)
+		g_cond_wait(data.cond, data.mutex);
+	g_mutex_unlock(data.mutex);
+
+	g_object_set(data.pipe, "uri", data.uri, NULL);
+	GstStateChangeReturn sc_ret = gst_element_set_state(
+		data.pipe, GST_STATE_PAUSED);
+	
+	if (sc_ret == GST_STATE_CHANGE_FAILURE) {
+		/* don't try this at home */
+		while (!data.error) g_thread_yield();
+		g_print("Invalid URI: %s\n", data.error->message);
+		goto fail_audio;
+	}
+
 	/* initialize curses */
 	initscr();
 
@@ -253,16 +368,19 @@ int main(int argc, char *argv[])
 	curs_set(0); /* this will break on unclean shutdown */
 	intro_screen();
 
-	g_object_set(data.pipe, "uri", data.uri, NULL);
-	gst_element_set_state(data.pipe, GST_STATE_PAUSED);
 	main_menu(&data);
 
 	endwin();
+
+fail_audio:
 	g_main_loop_quit(data.loop);
 	g_thread_join(gst_thread);
 	
 	g_free(data.uri);
 	gst_tag_list_free(data.tags);
+	g_cond_free(data.cond);
+	g_mutex_free(data.mutex);
+	if (data.error) g_error_free(data.error);
 	
 	return EXIT_SUCCESS;
 }
