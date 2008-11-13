538b28de43288d1c04735281c4c6be3550fc36ce
matt <matt@localhost>
2008-11-13 13:31:19 +0000

    * added script to generate deb script, and debian control files

---
+++ miniplay/trunk/makedeb.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+build_package() {
+	make DESTDIR=sandbox install
+	cp -r DEBIAN sandbox
+	dpkg -b sandbox .
+}
+
+if [ "$1" == "install" ]; then
+	build_package
+	exit 0
+fi
+
+autoreconf -i
+./configure --prefix=/usr CPPFLAGS=-DOLDGIO
+make
+fakeroot $0 install
