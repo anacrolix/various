ba0db11704ab71e1ec589c58ada7290b37a4f007
matt <matt@localhost>
2008-11-14 12:33:40 +0000

    * select_music(): fixed double free when none of the default music 	directories exist * initial music selection occurs as an event, rather than a dirty hack 	during initialization

--- miniplay/trunk/main.c
+++ miniplay/trunk/main.c
@@ -4,17 +4,21 @@ gchar const mp_app_name[] = "Miniplayer";
 
 gint main(gint argc, gchar *argv[])
 {
+	/* initialize libraries */
+	
 	gtk_init(&argc, &argv);
 	gst_init(&argc, &argv);
 	notify_init(mp_app_name);
 
 	g_set_prgname(mp_app_name);
+	
+	/* initialize our modules */
+	
 	init_audio();
 	init_tray();
 	mp_notify_init();
-
-	select_music();
-	connect_tray_signals();
+	
+	g_idle_add(select_music, NULL);
 
 	gtk_main();
 
