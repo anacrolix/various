dc8cb8dbe21acb7bb173a014033bb1865f78f6c0
matt <matt@localhost>
2008-12-06 13:32:55 +0000

    * opening file is asynchronous too * ReadUriAsync destroys itself before exiting through callback * ReadUriAsync's GInputStream is unref'd in the destructor * update_label handles NULL buffer case * online count label is set to "Querying" when reading * debian package dep on panel-applet * version 0.1.2 * fakeroot while building deb * include CFLAGS with g++

--- various/tiblet/readuriasync.hpp
+++ various/tiblet/readuriasync.hpp
@@ -2,40 +2,69 @@ template <typename UserData>
 class ReadUriAsync
 {
 public:
-	typedef void (*Callback)(gchar *, UserData, ReadUriAsync *);
+	typedef void (*Callback)(gchar *, UserData);
 
 	ReadUriAsync(
 			gchar const *uri,
 			Callback cb,
 			UserData ud)
 	:	ud_(ud),
+		cb_(cb),
 		gf_(g_file_new_for_uri(uri)),
-		count_(0),
-		cb_(cb)
+		gis_(NULL),
+		buf_(NULL),
+		count_(0)
 	{
 	}
 	~ReadUriAsync()
 	{
+		g_object_unref(gis_);
 		g_object_unref(gf_);
 	}
-	void readall(gssize max)
+
+	void start_reading(gssize max)
 	{
-		GInputStream *gis = reinterpret_cast<GInputStream *>(
-				g_file_read(gf_, NULL, NULL));
-		g_assert(gis);
-		buf_ = new gchar[max];
-		g_assert(buf_);
+		g_file_read_async(gf_, G_PRIORITY_DEFAULT, NULL, open_finish_cb, this);
 		max_ = max;
-		read(gis);
 	}
 
 private:
-	void read(GInputStream *gis)
+	static void end_reading(ReadUriAsync *rua)
+	{
+		gchar *buf = rua->buf_;
+		Callback cb = rua->cb_;
+		UserData ud = rua->ud_;
+		delete rua;
+		cb(buf, ud);
+	}
+
+	void read()
 	{
 		g_input_stream_read_async(
-				gis, buf_ + count_, max_ - count_,
+				gis_, buf_ + count_, max_ - count_,
 				G_PRIORITY_DEFAULT, NULL, read_finish_cb, this);
 	}
+
+	static void open_finish_cb(
+			GObject *source, GAsyncResult *result, gpointer userdata)
+	{
+		reinterpret_cast<ReadUriAsync *>(userdata)->open_finish(
+				reinterpret_cast<GFile *>(source), result);
+	}
+	void open_finish(GFile *gf, GAsyncResult *res)
+	{
+		gis_ = reinterpret_cast<GInputStream *>(
+				g_file_read_finish(gf, res, NULL));
+		if (gis_) {
+			buf_ = new gchar[max_];
+			g_assert(buf_);
+			read();
+		}
+		else {
+			end_reading(this);
+		}
+	}
+
 	void read_finish(GInputStream *gis, GAsyncResult *res)
 	{
 		gssize bytes = g_input_stream_read_finish(gis, res, NULL);
@@ -44,15 +73,15 @@ private:
 
 		if (bytes != 0) {
 			count_ += bytes;
-			read(gis);
+			read();
 			return;
 		}
 
-		g_object_unref(gis);
-		g_assert(count_ != max_);
+		g_assert(count_ <= max_);
+		if (count_ == max_) g_debug("filled entire buffer!");
 		buf_[count_] = '\0';
 
-		cb_(buf_, ud_, this);
+		end_reading(this);
 	}
 	static void read_finish_cb(
 			GObject *source, GAsyncResult *res, gpointer data)
@@ -64,6 +93,7 @@ private:
 	UserData ud_;
 	Callback cb_;
 	GFile *gf_;
+	GInputStream *gis_;
 	gchar *buf_;
 	gssize count_;
 	gssize max_;
