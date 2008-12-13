f1a3428dc98d5aabbcd5e8bd45d65575c4c71123
matt <matt@localhost>
2008-12-13 12:52:27 +0000

    * reworked the whole thing

--- various/ahocorasick/test.cpp
+++ various/ahocorasick/test.cpp
@@ -1,172 +1,55 @@
 #include "ahocorasick.hpp"
 
-//#include <fcntl.h>
+#include <cstdio>
 
-#include <fstream>
 #include <iostream>
-#include <set>
 #include <string>
 #include <vector>
 
 #include <boost/assign/list_of.hpp>
-#include <boost/assign/ptr_list_of.hpp>
 
 using namespace std;
 using namespace boost::assign;
-using namespace AC;
 
-class CStrKeyword : public Keyword<char>
+template <typename KeywordT>
+struct Results
 {
-public:
-	CStrKeyword(char const str[])
-	:	string_(str),
-		length_(strlen(str))
+	template <typename KeywordIterT>
+	Results(KeywordIterT const & begin, KeywordIterT const & end)
+	:	keywords_(begin, end),
+		hits_(distance(begin, end))
 	{
 	}
-	virtual size_t size() const { return length_; }
-	virtual char const &operator[](size_t index) const { return string_[index]; }
-#if !defined(NDEBUG)
-	virtual void debug_keyword() const {
-		debug("\"%s\"", string_);
+
+	void operator()(size_t where, size_t what)
+	{
+		cout << what << " : " << where << endl;
+		assert(hits_[what].insert(where).second);
+		cout << "offset: " << where << "; found: " << keywords_[what] << endl;
 	}
-	virtual void debug_symbol(size_t index) const {
-		debug("\"%c\"", string_[index]);
-	}
+
+	vector<KeywordT> keywords_;
+	vector<set<size_t> > hits_;
+};
+
+int main()
+{
+	vector<string> const keywords = list_of("he")("she")("his")("hers");
+	AhoCorasick<char> ac(keywords.begin(), keywords.end());
+	char input[] = "ushers";
+	Results<string> results(keywords.begin(), keywords.end());
+	ac.search(&input[0], &input[sizeof(input)], results);
+#if 0
+	vector<set<size_t> > const expected = list_of<set<size_t> >
+			(list_of(3))
+			(list_of(3))
+			()
+			(list_of(5));
 #endif
-private:
-	char const *string_;
-	size_t length_;
-};
-
-class WideStrKeyword : public Keyword<wchar_t>
-{
-public:
-	WideStrKeyword(wchar_t const str[])
-	:	string_(str),
-		length_(wcslen(str))
-	{
-	}
-	virtual size_t size() const { return length_; }
-	virtual wchar_t const &operator[](size_t index) const { return string_[index]; }
-#if !defined(NDEBUG)
-	virtual void debug_keyword() const {
-		wdebug(L"\"%s\"", string_);
-	}
-	virtual void debug_symbol(size_t index) const {
-		wdebug(L"\"%c\"", string_[index]);
-	}
-#endif
-private:
-	wchar_t const *string_;
-	size_t length_;
-};
-
-class CStrHaystack : public Haystack<char>
-{
-public:
-	CStrHaystack(char const hay[])
-	:
-		hay_(hay)
-	{}
-
-	virtual bool at(size_t offset, char const *&input)
-	{
-		if (offset < strlen(hay_)) {
-			input = &hay_[offset];
-			return true;
-		}
-		else
-			return false;
-	}
-private:
-	char const *hay_;
-};
-
-class BinaryFileHaystack : public Haystack<char>
-{
-public:
-	BinaryFileHaystack(char const *filename)
-	:	Haystack<char>()
-	{
-		ifs_.open(filename);
-	}
-	~BinaryFileHaystack()
-	{
-		ifs_.close();
-	}
-
-	virtual bool at(size_t offset, char const *&input)
-	{
-		if (ifs_.good()) {
-			ifs_.get(buf_);
-			input = &buf_;
-			return true;
-		}
-		else
-			return false;
-	}
-
-private:
-	ifstream ifs_;
-	mutable char buf_;
-};
-
-static void print_hit(size_t index, Keyword<char> const *keyword)
-{
-	debug("Found hit [%zu]: ", index);
-	keyword->debug_keyword();
-	debug("\n");
-}
-
-bool test1()
-{
-	vector<Keyword<char> *> kw;
-	kw.push_back(new CStrKeyword("he"));
-	kw.push_back(new CStrKeyword("she"));
-	kw.push_back(new CStrKeyword("his"));
-	kw.push_back(new CStrKeyword("hers"));
-
-	CStrHaystack hay("ushers");
-
-	AhoCorasick<char> ac(kw.begin(), kw.end());
-	ac(hay, print_hit);
-	ac(hay, print_hit);
-
-	return false;
-}
-
-bool test2(char const *filename)
-{
-	vector<Keyword<char> *> kw;
-	kw.push_back(new CStrKeyword("vector"));
-	kw.push_back(new CStrKeyword("vec"));
-	kw.push_back(new CStrKeyword("vecna"));
-	//kw.push_back(new CStrKeyword("mage"));
-	kw.push_back(new CStrKeyword("fighter"));
-	kw.push_back(new CStrKeyword("god"));
-	kw.push_back(new CStrKeyword("sword"));
-	kw.push_back(new CStrKeyword("spell"));
-	kw.push_back(new CStrKeyword("body"));
-	kw.push_back(new CStrKeyword("matt"));
-	kw.push_back(new CStrKeyword("04"));
-	kw.push_back(new CStrKeyword("table"));
-	kw.push_back(new CStrKeyword("and"));
-	//kw.push_back(new CStrKeyword("x"));
-
-	vector<Keyword<wchar_t> *> kw2;
-	kw2.push_back(new WideStrKeyword(L"hi"));
-
-	BinaryFileHaystack hay(filename);
-	AhoCorasick<char> ac(kw.begin(), kw.end());
-	ac(hay, print_hit);
-
-	return false;
-}
-
-int main(int argc, char **argv)
-{
-	test1();
-	test2(argv[1]);
+	vector<set<size_t> > expected(4);
+	expected[0].insert(3);
+	assert(expected.size() == results.hits_.size());
+	assert(expected[0].size() == results.hits_[0].size());
 
 	return 0;
 }
