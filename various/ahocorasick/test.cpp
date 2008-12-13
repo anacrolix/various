85350102bb6681608ce78f3659a93125015a9151
matt <matt@localhost>
2008-12-13 15:40:11 +0000

    * generalize test functions * test performance on files * switch to -O3 * makeinput was generating 4x the bytes required

--- various/ahocorasick/test.cpp
+++ various/ahocorasick/test.cpp
@@ -2,6 +2,7 @@
 
 #include <cstdio>
 
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -30,10 +31,10 @@ struct Results
 	vector<set<size_t> > hits_;
 };
 
-typedef vector<set<size_t> > (*correctness_test)(vector<string> const &, char const *, char const *);
+typedef vector<set<size_t> > (*test_function_t)(vector<string> const &, char const *, char const *);
 
 vector<set<size_t> >
-matt_correctness(vector<string> const & keywords, char const *begin, char const *end)
+matt_test_function(vector<string> const & keywords, char const *begin, char const *end)
 {
 	AhoCorasick<char> ac(keywords.begin(), keywords.end());
 	Results <vector<string> > results(keywords);
@@ -41,7 +42,7 @@ matt_correctness(vector<string> const & keywords, char const *begin, char const 
 	return results.hits_;
 }
 
-static vector<correctness_test> correctness_tests = list_of(matt_correctness);
+static vector<test_function_t> test_functions = list_of(matt_test_function);
 
 int main()
 {
@@ -53,10 +54,27 @@ int main()
 			()
 			(list_of(5));
 
-	for (vector<correctness_test>::iterator it = correctness_tests.begin();
-		it != correctness_tests.end(); ++it)
+	for (vector<test_function_t>::iterator it = test_functions.begin();
+		it != test_functions.end(); ++it)
 	{
 		assert(expected == (**it)(keywords, &input[0], &input[sizeof(input)]));
+	}
+
+	{
+		vector<string> const file_kw = list_of("sex")("dick")("c++");
+		AhoCorasick<char> matt_ac(file_kw.begin(), file_kw.end());
+		Results<vector<string> > results(file_kw);
+		ifstream fag("input", ifstream::binary);
+		while (fag.good()) {
+#if 1
+			char buf[512];
+			fag.read(buf, 512);
+			matt_ac.search(buf, buf + fag.gcount(), results);
+#else
+			char c = fag.get();
+			matt_ac.search(&c, &c + 1, results);
+#endif
+		}
 	}
 
 	return 0;
