35e0dad7c88fe580ceca275d1712e22b76521370
matt <matt@localhost>
2008-12-15 12:11:05 +0000

    * attempt to give eric's code first aid...

--- various/ahocorasick/eric/erisick.h
+++ various/ahocorasick/eric/erisick.h
@@ -8,12 +8,44 @@ class erisick
 {
 public:
 	erisick();
-	//erisick(erisick *Parent);
-	//erisick(erisick *Parent, char c);
+	erisick(erisick *Parent);
+	erisick(erisick *Parent, char c);
 	void add(std::list<std::string> needles);
-	template <typename CallbackT>
-	void search(char *start, char *end, CallbackT & callback);
 
+	template <typename CallbackT>
+	void search(char *start, char *end, CallbackT & callback)
+	{
+		std::list<std::pair<int, std::string> > results;
+		erisick *at = this;
+
+		for(char *i = start; i < end ; i++)
+		{
+			erisick *cor = NULL;
+			while(cor == NULL)
+			{
+				cor = at->kid[*i];
+
+				if (at == this) //no use for us
+					break;
+
+				if(cor == NULL) //didn't find
+					at = at->fallback; //and repeat
+			}
+
+			if(cor != NULL)
+				at = cor;
+
+
+			for(std::list<std::string>::iterator f = at->found.begin(); f != at->found.end(); f++)
+			{
+				size_t where = i-start;
+				//*f is the std::string we found
+				callback(where, *f); 
+			}
+
+		}
+
+	}
 
 	erisick *parent;
 	erisick *fallback;
