f0c27495173608a2aee1cb605b6900cfbd12bac5
eric <eric@localhost>
2008-12-16 10:43:33 +0000

    Getting close!!!!!!!!!

--- various/ahocorasick/eric/erisick.h
+++ various/ahocorasick/eric/erisick.h
@@ -10,7 +10,41 @@ public:
 
 	void add(std::vector<std::string> needles);
 
-	void search(char *start, char *end, void (*callback)(std::string, size_t));
+        template <typename CallbackT>
+	void search(char *start, char *end, CallbackT & callback)
+        {
+	erisick *at = this;
+
+	for(char *i = start; i < end ; i++)
+	{
+		erisick *cor = NULL;
+		while(cor == NULL)
+		{
+			cor = at->kid[*i];
+
+			if (at == this) //no use for us
+				break;
+
+			if(cor == NULL) //didn't find
+				at = at->fallback; //and repeat
+		}
+
+		if(cor != NULL)
+			at = cor;
+
+		if(!at->found.empty()) //should be slightly faster
+		{
+		for(std::vector<std::string>::iterator f = at->found.begin(); f != at->found.end(); f++)
+		{
+			size_t where = i-start;
+			//*f is the std::string we found
+			callback(*f, where);
+		}
+		}
+
+	}
+
+    }
 
 	erisick *parent;
 	erisick *fallback;
