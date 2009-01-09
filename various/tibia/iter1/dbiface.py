6e83bbd2139fdb4a64cf988d94edaa09d3edad01
matt <matt@localhost>
2009-01-09 09:54:47 +0000

    * prepare for new iteration of tibia website parsing

--- various/tibia/iter1/dbiface.py
+++ various/tibia/iter1/dbiface.py
@@ -3,7 +3,7 @@ import time
 
 TIBIADB_SCHEME = \
 """
-create table if not exists vocation (
+create table vocation (
 	id integer primary key,
 	str text unique
 );
@@ -18,42 +18,52 @@ insert or ignore into vocation (str) values ('Elder Druid');
 insert or ignore into vocation (str) values ('Sorcerer');
 insert or ignore into vocation (str) values ('Master Sorcerer');
 
-create table if not exists player (
+create table player (
 	id integer primary key,
 	name text unique,
-	level integer,
-	voc_id integer
+	monster integer,
 );
-create table if not exists online (
+create table online (
 	plyr_id integer,
 	level integer,
 	voc_id integer,
-	time text
+	ontime text
+);
+create table death (
+	victim_id integer primary key,
+	time text primary key,
+	level integer,
+	slayer_id integer primary key,
+	assist integer primary key
 );
 """
 
 class TibiaDB():
-	def __init__(self):
-		self.conn = sqlite3.connect("tibiadb")
+	def __init__(self, dbpath="tibiadb"):
+		self.conn = sqlite3.connect(dbpath)
 		self.curs = self.conn.cursor()
-		self.curs.executescript(TIBIADB_SCHEME)
+		# it's okay if the schema is already created
+		try:
+			self.curs.executescript(TIBIADB_SCHEME)
+		except sqlite3.OperationalError:
+			pass
 
 	def __del__(self):
 		self.conn.commit()
 
 	def __get_player_id(self, name, insert=True):
-		self.curs.execute("select id from player where name=?", (name,))
+		self.curs.execute("select id from player where name=?", [name])
 		r = self.curs.fetchone()
 		if r is None:
 			assert insert is True
-			self.curs.execute("insert into player (name) values (?)", (name,))
+			self.curs.execute("insert into player (name) values (?)", [name])
 			id = self.__get_player_id(name, False)
 		else:
 			id = r[0]
 		return id
 
 	def __get_vocation_id(self, vocation):
-		self.curs.execute("select id from vocation where str=?", (vocation,))
+		self.curs.execute("select id from vocation where str=?", [vocation])
 		# this can't fail or the vocation doesn't exist
 		return self.curs.fetchone()[0]
 
@@ -70,7 +80,7 @@ class TibiaDB():
 
 		# update online presence
 		if infos["online"] is True:
-			self.curs.execute("insert into online (plyr_id, level, voc_id, time) values (?,?,?,datetime(?, 'unixepoch'))", (plyr_id, infos["level"], voc_id, infos["time"]))
+			self.curs.execute("insert into online (plyr_id, level, voc_id, ontime) values (?,?,?,datetime(?, 'unixepoch'))", (plyr_id, infos["level"], voc_id, infos["ontime"]))
 
 		# update player details
 		cols_to_update = {"voc_id": voc_id}
@@ -81,5 +91,5 @@ class TibiaDB():
 			self.curs.execute(r"update player set %s=? where id=?" % (k,), (v, plyr_id))
 
 	def get_recent_online_players(self, seconds):
-		self.curs.execute("select distinct player.name from player,online where player.id=online.plyr_id and online.time > datetime(?, 'unixepoch')", (time.time() - seconds,))
+		self.curs.execute("select distinct player.name from player,online where player.id=online.plyr_id and online.ontime > datetime(?, 'unixepoch')", (time.time() - seconds,))
 		return self.curs.fetchall()
