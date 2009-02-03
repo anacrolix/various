a032d46a424f666bb838578491c9e24b1117d89c
matt <matt@localhost>
2009-02-03 08:53:52 +0000

    * updated internode-applet to build on other platforms

--- debian_packages/internode-applet/makedeb.sh
+++ debian_packages/internode-applet/makedeb.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-wget -c http://www.users.on.net/~spohlenz/internode/internode-applet-1.6.tar.gz
-tar -xvzf internode-applet-1.6.tar.gz
-cd internode-applet-1.6
-python setup.py install --root=../debroot
+PACKAGE_NAME='internode-applet-1.6'
+STAGE_DIR='debroot'
+BUILD_ARCH=$(dpkg-architecture -qDEB_BUILD_ARCH)
+
+wget -c http://www.users.on.net/~spohlenz/internode/${PACKAGE_NAME}.tar.gz
+tar -xzf ${PACKAGE_NAME}.tar.gz
+cd ${PACKAGE_NAME}
+python setup.py install --root=../${STAGE_DIR}
 cd ..
-mkdir -p debroot/DEBIAN
-cp control debroot/DEBIAN
-dpkg-deb -b debroot .
+mkdir -p ${STAGE_DIR}/DEBIAN
+sed "s/@BUILD_ARCH@/${BUILD_ARCH}/" control > ${STAGE_DIR}/DEBIAN/control
+dpkg-deb -b ${STAGE_DIR} .
+
