e3db3f8401d859ec7156e8d3637ccf4440efa77b
anacrolix <anacrolix@localhost>
2009-02-28 12:53:02 +0000

    taskset: Clean-up and comments.

--- utility/c++/taskset/test/test.cpp
+++ utility/c++/taskset/test/test.cpp
@@ -39,12 +39,20 @@ int main()
 	int step = 1000000;
 	for (int min = 0; min < 4000000000; min += step)
 		sums.push_back(boost::shared_ptr<Sum>(new Sum(min + 1, min + step)));
-	taskset.add_tasks(sums.begin(), sums.end());
+	taskset.add_tasks(sums.begin(), sums.begin() + 5);
+	boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
+	taskset.add_tasks(sums.begin() + 5, sums.begin() + 1000);
+	boost::this_thread::sleep(boost::posix_time::milliseconds(3000));
 	taskset.wait_empty();
+	taskset.wait_empty();
+	taskset.add_tasks(sums.begin() + 1000, sums.begin() + 2000);
+	taskset.add_tasks(sums.begin() + 2000, sums.end());
 	int total = 0;
+	taskset.wait_empty();
 	for (std::deque<boost::shared_ptr<Sum> >::const_iterator it = sums.begin(); it != sums.end(); ++it)
 		total += (*it)->result();
 	std::cout << "total: " << total << "\n";
+	// obscene overflow
 	assert(total == -475645216);
 	system("pause");
 	return 0;
