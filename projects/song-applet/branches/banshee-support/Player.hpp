19c3ef856cc0f97c9331774fac782cabdded1426
anacrolix <anacrolix@localhost>
2009-03-07 11:05:22 +0000

    More work on song-applet banshee support and C++ conversion.

--- projects/song-applet/branches/banshee-support/Player.hpp
+++ projects/song-applet/branches/banshee-support/Player.hpp
@@ -5,40 +5,48 @@
 class Player
 {
 public:
-    Player(DBusGProxy *dbus)
-    :   active_(dbus_has_owner(dbus))
+    Player(DBusGProxy *dbus, char const *service)
+    :   active_(dbus_has_owner(dbus, service))
     {
     }
 
-    gboolean dbus_has_owner(DBusGProxy *dbus)
+    gboolean dbus_has_owner(DBusGProxy *dbus, char const *service)
     {
         gboolean has_owner;
         // BOOLEAN NameHasOwner (in STRING name)
         dbus_g_proxy_call(
                 dbus, "NameHasOwner", NULL,
-                G_TYPE_STRING, this->get_service_name(), G_TYPE_INVALID,
+                G_TYPE_STRING, service, G_TYPE_INVALID,
                 G_TYPE_BOOLEAN, &has_owner, G_TYPE_INVALID);
         return has_owner;
     }
 
-    void set_active(bool active)
+    bool service_owner_changed(char const *service, char const *newowner)
     {
-        active_ = active;
+        bool active(active_);
+        if (g_strcmp0(service, this->get_service_name()) == 0)
+        {
+            if (g_strcmp0(newowner, "") != 0) {
+                active_ = true;
+            } else {
+                active_ = false;
+            }
+        }
+        return active != active_;
     }
 
+    virtual char const *get_service_name() const = 0;
     bool get_active() const { return active_; }
-
-    virtual gchar const *get_service_name() const = 0;
 
 protected:
     static DBusGProxy *get_dbus_g_proxy(
-            DBusGConnection *dbusgconn,
+            DBusGConnection *bus,
             char const *name,
             char const *path,
             char const *interface)
     {
         DBusGProxy *proxy = dbus_g_proxy_new_for_name(
-                dbusgconn, name, path, interface);
+                bus, name, path, interface);
         g_assert(proxy);
         return proxy;
     }
