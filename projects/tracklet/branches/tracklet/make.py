75b35e3bfd0d87877ef96b49eaf13d7164fda1ec
anacrolix <anacrolix@localhost>
2009-04-04 02:19:20 +0000

    Add auxiliary configuration file relationship.

--- projects/tracklet/branches/tracklet/make.py
+++ projects/tracklet/branches/tracklet/make.py
@@ -11,18 +11,22 @@ else:
     assert False
 
 OBJECTS = "main.o Applet.o RhythmboxProxy.o".split(" ")
+AUXCONFS = "control symbols Makefile tracklet.server config.h".split(" ")
+AUXCONF_DEPS = map(lambda x: x + ".in", AUXCONFS)
 PCLIBS = "dbus-glib-1 gtk+-2.0 libpanelapplet-2.0"
 PCLIB_LDFLAGS = BuildVariable(["pkg-config", "--libs", PCLIBS], shell=False)
 PCLIB_CFLAGS = BuildVariable(["pkg-config", "--cflags", PCLIBS], shell=False)
 
 link_step = BuildStep(lambda x, y: [CC, PCLIB_LDFLAGS, "-o", x[0]] + y)
 compile_step = BuildStep(lambda x, y: [CC, PCLIB_CFLAGS, "-c", "-o", x[0], y[0]])
-#configure_step =
+configure_step = BuildStep(lambda x, y: ["./configure"])
 
 binary = Relationship(["tracklet"], OBJECTS, link_step)
+auxconf = Relationship(AUXCONFS, AUXCONF_DEPS, configure_step)
 PatternRule("\.o$", ".cpp", compile_step)
 
+auxconf.update()
 binary.update()
-#PCLIB_CFLAGS = Variable("
 
+print AUXCONF_DEPS
 print "FINISH"
