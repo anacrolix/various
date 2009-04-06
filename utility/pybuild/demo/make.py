9e0dc2feaa4487fbf13fcb21eb07be8946cf5c4a
anacrolix <anacrolix@localhost>
2009-04-06 10:54:55 +0000

    Provide dummy implementation of curses if it doesn't exist. MSVC was hating on .o extension in the demo.

--- utility/pybuild/demo/make.py
+++ utility/pybuild/demo/make.py
@@ -8,6 +8,7 @@ from pybuild import *
 # this is really good bit...
 if os.name == 'posix':
     CC = "gcc"
+    OBJECT_SUFFIX = ".o"
     compile_args = lambda ts, ds: [CC, "-c", "-o", ts[0], ds[0]]
     link_args = lambda ts, ds: [CC, "-o", ts[0]] + ds
 elif os.name == 'nt':
@@ -16,6 +17,7 @@ elif os.name == 'nt':
     # running this .bat file "%VS90COMNTOOLS%"vsvars32.bat, sets up the environment,
     # but how can we do this before the build script is started?
     CC = "cl"
+    OBJECT_SUFFIX = ".obj"
     compile_args = lambda ts, ds: [CC, "/c", "/Fo"+ts[0], ds[0], "/nologo"]
     # cl translates to link's even more bizarre parameters for us
     link_args = lambda ts, ds: [CC, "/Fe"+ts[0], "/nologo"] + ds
@@ -23,9 +25,10 @@ else:
     # if you are a developer using a mac, seek professional help
     assert False
 
-OBJECTS = "demo_a.o demo_b.o".split()
+OBJECTS = map(lambda o: o + OBJECT_SUFFIX, "demo_a demo_b".split())
 # how to build .o files from .c files
-PatternRule(".*\.o$", BuildStep(compile_args), lambda x: ([x], [re.sub("\.o$", ".c", x)]))
+
+PatternRule(r".*" + re.escape(OBJECT_SUFFIX) + r"$", BuildStep(compile_args), lambda x: ([x], [re.sub(re.escape(OBJECT_SUFFIX) + r"$", ".c", x)]))
 # how to build, and what our binary depends on
 binary = Relationship(["demo.exe"], OBJECTS, BuildStep(link_args))
 
