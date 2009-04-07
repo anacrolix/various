16e2504ea3e4f7eda342f5c04952e024037162c2
anacrolix <anacrolix@localhost>
2009-04-07 11:39:11 +0000

    Rename poc to proof_of_concept, it was unclear. Remove obsoleted configure bash script, and symbols.in file.

--- projects/tracklet/branches/tracklet/make.py
+++ projects/tracklet/branches/tracklet/make.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
+from pybake import *
+
+import os
 import re
 import sys
-from pybuild import *
+import tempfile
 
 CC = "g++"
 OBJECTS = "main.o Applet.o RhythmboxProxy.o".split()
-AUXCONFS = "control symbols Makefile tracklet.server config.h".split()
+AUXCONFS = "control Makefile tracklet.server config.h".split()
 # auxiliary conf files are made from corresponding .in file
 AUXCONF_DEPS = map(lambda x: x + ".in", AUXCONFS)
 # the package config dependencies
@@ -17,6 +20,21 @@ PCLIB_CFLAGS = Variable(["pkg-config", "--cflags"] + PCLIBS)()
 link_step = BuildStep(lambda x, y: [CC, "-o", x[0]] + y + PCLIB_LDFLAGS.split())
 compile_step = BuildStep(lambda x, y: [CC, "-c", "-o", x[0], y[0]] + PCLIB_CFLAGS.split())
 configure_step = BuildStep(lambda x, y: ["./configure"])
+
+symbols = [
+    ("PREFIX", "/usr"),
+    ("PACKAGE_VERSION", "0.2"),
+    ("PACKAGE_REVISION", "0"),
+    ("PACKAGE_NAME", "tracklet"),
+    ("APPLET_FULLNAME", "Tracklet"),
+    ("APPLET_DESCRIPTION", "Provides an icon to remove the current playing track from disk"),
+    ("BONOBO_FACTORY_IID", "@BONOBO_APPLET_IID@_Factory"),
+    ("BONOBO_APPLET_IID", "OAFIID:Tracklet"),
+]
+
+# BUILD_ARCH = `dpkg-architecture -qDEB_BUILD_ARCH`
+
+
 def cpp_depgen(target):
     srcdep = re.sub("\.o$", ".cpp", target)
     makerule = Variable([CC, "-MM", "-MG", "-MT", target, srcdep])()
@@ -25,7 +43,8 @@ def cpp_depgen(target):
     return maketargs, makedeps
 
 binary = Relationship(["tracklet"], OBJECTS, link_step)
-auxconf = Relationship(AUXCONFS, AUXCONF_DEPS, configure_step)
+for auxconf in AUXCONFS:
+    Relationship([auxconf], [auxconf + ".in"], Configure(symbols))
 PatternRule(".*\.o$", compile_step, cpp_depgen)
 
-binary.update_all()
+Relationship.update_all()
