e4cf59ac6841208fda89fb828bfe7fdcca8fae97
anacrolix <anacrolix@localhost>
2009-04-15 14:28:25 +0000

    Start adapting pybake to conway.

--- utility/pybake/__init__.py
+++ utility/pybake/__init__.py
@@ -1,5 +1,10 @@
+import sys
+sys.path.append(".")
+
 from classes import BuildStep, Configure, ExplicitRule, ImplicitRule, PhonyRule, SystemTask
+from compiler import *
 from functions import pybake_main, clean_targets
+
 import buildsys
 
 functions.initialize()
---
+++ utility/pybake/compiler/msvc.py
@@ -0,0 +1,31 @@
+﻿from pybake.classes import Command, SystemTask
+
+class Compiler(Command):
+	def __init__(self, defines=None, includes=None):
+		self.defines = defines
+		self.includes = includes
+	def __call__(self, outputs, inputs):
+		assert len(outputs) == 1
+		assert len(inputs) == 1
+		args = ["cl", "/c", "/EHsc", "/MD", "/nologo"]
+		args += ["/Fo" + outputs[0]]
+		args += inputs
+		args += reduce(lambda y, z: y + z, [("/D", x) for x in self.defines])
+		args += [("/I", x) for x in self.includes]
+		SystemTask(args)()
+		return True
+		
+class Linker(Command):
+	def __init__(self, libpaths=[]):
+		self.libpaths = libpaths
+	def __call__(self, outputs, inputs):
+		assert len(outputs) == 1
+		args = ["link", "/SUBSYSTEM:CONSOLE", "/NOLOGO"]
+		args += ["/OUT:" + outputs[0]]
+		args += ["/LIBPATH:" + x for x in self.libpaths]
+		args += inputs
+		SystemTask(args)()
+		return True
+		
+		
+	
