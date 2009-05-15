007b2b36e30f21e71bd8e2612bab337ebe58d988
anacrolix <anacrolix@localhost>
2009-05-15 17:06:11 +0000

    Indentation changes.

--- projects/tibia/iter3/tibiacom.py
+++ projects/tibia/iter3/tibiacom.py
@@ -47,13 +47,13 @@ def pretty_print_char_info(info):
     simple = info.keys()
     for a in ["deaths", "name"]: simple.remove(a)
     for k in ["name"] + simple:
-            print k + ":", info[k],
-            if k in ["created", "last login"] and info[k] is not None:
-                    print "(" + str(int(time.time()) - tibia_time_to_unix(info[k])) + "s ago)"
-            elif k is "timestamp":
-                    print "(" + time.asctime(time.gmtime(info[k] + 3600)), "CET)"
-            else:
-                    print
+        print k + ":", info[k],
+        if k in ["created", "last login"] and info[k] is not None:
+            print "(" + str(int(time.time()) - tibia_time_to_unix(info[k])) + "s ago)"
+        elif k is "timestamp":
+            print "(" + time.asctime(time.gmtime(info[k] + 3600)), "CET)"
+        else:
+            print
     a = info["deaths"]
     if a != None:
         for b in a:
@@ -66,72 +66,72 @@ def http_get(url, params):
         return http_get(url, params)
 
 def __ci_info(html):
-        FIELDS = (
-                        ("name", "Name", lambda x: unescape_tibia_html(x)),
-                        ("sex", "Sex", None),
-                        ("vocation", "Profession", None),
-                        ("level", "Level", None),
-                        ("world", "World", None),
-                        ("residence", "Residence", None),
-                        ("guild", "Guild&#160;membership", lambda x: unescape_tibia_html(re.search(r"<A.+?>([^<]+)</A>", x).group(1))),
-                        ("last login", "Last login", lambda x: unescape_tibia_html(x)),
-                        ("account status", "Account&#160;Status", None),
-                        ("created", "Created", lambda x: unescape_tibia_html(x)))
-        info = {}
-        for a in FIELDS:
-                hits = re.findall(
-                                r"<TR.*?><TD.*?>%s:</TD><TD>(.+?)</TD></TR>" % a[1],
-                                html)
-                if len(hits) == 0:
-                        info[a[0]] = None
-                elif len(hits) == 1:
-                        info[a[0]] = a[2](hits[0]) if a[2] != None else hits[0]
-                else:
-                        assert False
-        return info
+    FIELDS = (
+        ("name", "Name", lambda x: unescape_tibia_html(x)),
+        ("sex", "Sex", None),
+        ("vocation", "Profession", None),
+        ("level", "Level", None),
+        ("world", "World", None),
+        ("residence", "Residence", None),
+        ("guild", "Guild&#160;membership", lambda x: unescape_tibia_html(re.search(r"<A.+?>([^<]+)</A>", x).group(1))),
+        ("last login", "Last login", lambda x: unescape_tibia_html(x)),
+        ("account status", "Account&#160;Status", None),
+        ("created", "Created", lambda x: unescape_tibia_html(x)))
+    info = {}
+    for a in FIELDS:
+        hits = re.findall(
+            r"<TR.*?><TD.*?>%s:</TD><TD>(.+?)</TD></TR>" % a[1],
+            html)
+        if len(hits) == 0:
+            info[a[0]] = None
+        elif len(hits) == 1:
+            info[a[0]] = a[2](hits[0]) if a[2] != None else hits[0]
+        else:
+            assert False
+    return info
 
 def __ci_deaths(html):
-        def slayer_tuple(group):
-                assert len(group) == 2
-                if group[0] == None:
-                        assert group[1] != None
-                        return (False, group[1])
-                else:
-                        assert group[1] == None
-                        return (True, unescape_tibia_html(group[0]))
+    def slayer_tuple(group):
+        assert len(group) == 2
+        if group[0] == None:
+            assert group[1] != None
+            return (False, group[1])
+        else:
+            assert group[1] == None
+            return (True, unescape_tibia_html(group[0]))
 
-        # navigate to start of deaths table
-        m = re.search("""<TABLE.*?>Character Deaths.*?</TR>""", html)
-        if m == None: return []
-        pos = m.end()
+    # navigate to start of deaths table
+    m = re.search("""<TABLE.*?>Character Deaths.*?</TR>""", html)
+    if m == None: return []
+    pos = m.end()
 
-        CHAR_OR_MONSTER_PATTERN = """(?:<A.*?>([^<]*)</A>|([^<]*))"""
-        # time, level, killer
-        kill1_re = re.compile(
-                        """<TR.*?><TD.*?>([^<]+)</TD><TD>\S+ at Level (\d+) by """
-                        + CHAR_OR_MONSTER_PATTERN
-                        + """</TD></TR>""")
-        # killer
-        kill2_re = re.compile(
-                        """\s*<TR.*?><TD.*?></TD><TD>and by """
-                        + CHAR_OR_MONSTER_PATTERN
-                        + """</TD></TR>""")
+    CHAR_OR_MONSTER_PATTERN = """(?:<A.*?>([^<]*)</A>|([^<]*))"""
+    # time, level, killer
+    kill1_re = re.compile(
+            """<TR.*?><TD.*?>([^<]+)</TD><TD>\S+ at Level (\d+) by """
+            + CHAR_OR_MONSTER_PATTERN
+            + """</TD></TR>""")
+    # killer
+    kill2_re = re.compile(
+            """\s*<TR.*?><TD.*?></TD><TD>and by """
+            + CHAR_OR_MONSTER_PATTERN
+            + """</TD></TR>""")
 
-        deaths = []
-        while True:
-                mo = kill1_re.search(html, pos)
-                if mo is None: break
-                time = unescape_tibia_html(mo.group(1))
-                level = mo.group(2)
-                death = [time, level, slayer_tuple(mo.group(3, 4)), None]
-                pos = mo.end()
-                mo2 = kill2_re.match(html, pos)
-                if mo2 is not None:
-                        death[3] = slayer_tuple(mo2.group(1, 2))
-                        pos = mo2.end()
-                deaths.append(tuple(death))
+    deaths = []
+    while True:
+        mo = kill1_re.search(html, pos)
+        if mo is None: break
+        time = unescape_tibia_html(mo.group(1))
+        level = mo.group(2)
+        death = [time, level, slayer_tuple(mo.group(3, 4)), None]
+        pos = mo.end()
+        mo2 = kill2_re.match(html, pos)
+        if mo2 is not None:
+            death[3] = slayer_tuple(mo2.group(1, 2))
+            pos = mo2.end()
+        deaths.append(tuple(death))
 
-        return deaths
+    return deaths
 
 def char_info(name):
     rv = {"timestamp": int(time.time())}
