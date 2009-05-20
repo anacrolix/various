1b0e9b248a0b38513efc6a7e2cb5ee910a7077b5
anacrolix <anacrolix@localhost>
2009-05-20 13:28:33 +0000

    Rename prolepsis "source", "prolepsis". Load listbox font preferences from a config file.

--- projects/prolepsis/trunk/prolepsis.py
+++ projects/prolepsis/trunk/prolepsis.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python
 
-import pdb
-import sys
-import Tkinter
-sys.path.append("source")
-from source.classes import MainDialog
-
-root = Tkinter.Tk()
-main_dialog = MainDialog(root)
-root.mainloop()
-
-print "exited mainloop cleanly."
+from prolepsis import main
+main()
