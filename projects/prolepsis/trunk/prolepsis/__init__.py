6fad3c7d04105a99758cc0bd36a7f93cdf07ce68
anacrolix <anacrolix@localhost>
2009-05-21 13:35:02 +0000

    PROLEPSIS: Move a lot of config stuff into globals. Use a local threadpool to parse for PZ locks. When refreshing listbox, iterate through a copy of the char data incase the array size changes. Fix bug where a character who has no deaths in his record cannot show up as PZ locked. Don't flip fg/bg for a PZ locked character. Instead just set the background to black, and ensure the foreground is visible. Load stance colors from config file. Ignore local config and shelf files in SVN.

--- projects/prolepsis/trunk/prolepsis/__init__.py
+++ projects/prolepsis/trunk/prolepsis/__init__.py
@@ -1,31 +1,7 @@
-import os
-import pdb
-import sys
-import ConfigParser
 import Tkinter
 import tkFont
 import dialogs
-
-config = ConfigParser.RawConfigParser()
-config.add_section("listbox font")
-config.set("listbox font", "size", 9)
-config.set("listbox font", "weight", tkFont.BOLD)
-try:
-    config.set("listbox font", "family", {
-            "posix": "Monospace",
-            "nt": "Courier New"
-        }[os.name])
-except KeyError:
-    pass
-
-CONFNAME = "prolepsis.conf"
-
-config_file = open(CONFNAME, "r+" if os.path.exists(CONFNAME) else "w+")
-#config.readfp(open(CONFNAME, "r+"))
-config.readfp(config_file)
-config_file.seek(0)
-config.write(config_file)
-config_file.close()
+from globals import config
 
 root = Tkinter.Tk()
 listbox_font = tkFont.Font(**dict(config.items("listbox font")))
