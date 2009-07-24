3e8a900186de52681f7521165aac7d9ac52be55f
anacrolix <anacrolix@localhost>
2009-07-24 17:57:39 +0000

    BENCHMARK: Add python str.find() wrapper. Optimize horspool.

--- algorithms/benchmark/src/main.cc
+++ algorithms/benchmark/src/main.cc
@@ -3,8 +3,10 @@
 #include <sstream>
 #include <vector>
 
+using namespace std;
+
 size_t SGM_FILE_COUNT = 1;
-std::vector<size_t> SGM_TOTAL_BYTES(22);
+vector<size_t> SGM_TOTAL_BYTES(22);
 
 int main(int argc, char **argv)
 {
@@ -13,11 +15,15 @@ int main(int argc, char **argv)
     testing::InitGoogleTest(&argc, argv);
     if (argc == 2)
     {
-        std::stringstream(argv[1]) >> SGM_FILE_COUNT;
+		std::stringstream ss(argv[1]);
+		ss.exceptions(ios::badbit);
+		ss >> SGM_FILE_COUNT;
+		if (!ss.eof()) throw exception();
     }
     else assert(argc == 1);
     assert(SGM_FILE_COUNT >= 1 && SGM_FILE_COUNT <= 22);
     SGM_TOTAL_BYTES.at(0) = 1324350;
+	SGM_TOTAL_BYTES.at(2) = 3796285;
     SGM_TOTAL_BYTES.at(21) = 27636766;
     int retcode = RUN_ALL_TESTS();
 #ifdef WIN32
