1e27b38a4c5a0121ee47cbd41f5e44864b23173d
anacrolix <anacrolix@localhost>
2009-07-25 09:30:52 +0000

    BENCHMARK: Removed inappropriate svn:executable bits. Updated reuters.py to fetch the package. Updated README.

--- algorithms/benchmark/README.txt
+++ algorithms/benchmark/README.txt
@@ -1,2 +1,13 @@
+BUILDING
+
 windows:
  * if pyconfig.h is missing, you can get it from the source package in PC/
+ * debug configuration will compile against release build, this can be changed
+
+linux:
+ * you will need something like: python-dev, boost~filesystem & headers, g++
+
+RUNNING
+
+ * accepts the gtest parameters, plus an optional number of SGM files to search (from 000 to the number you give)
+ * run reuters.py to download and extract the expected SGM scan targets and keyword lists
--- algorithms/benchmark/reuters.py
+++ algorithms/benchmark/reuters.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 
-reuters_file = open("reuters21578.tar.gz", "rb")
+from urllib import urlretrieve
 import hashlib
+import os.path
+import tarfile
+
+REUTERS_ARCHIVE = "reuters21578.tar.gz"
+REUTERS_ARCHIVE_URL = "http://www.daviddlewis.com/resources/testcollections/reuters21578/reuters21578.tar.gz"
+
+if not os.path.exists(REUTERS_ARCHIVE):
+    print "Downloading", REUTERS_ARCHIVE
+    urlretrieve(REUTERS_ARCHIVE_URL, REUTERS_ARCHIVE)
+reuters_file = open(REUTERS_ARCHIVE, "rb")
 assert hashlib.md5(reuters_file.read()).hexdigest() == "a22faadd7dde0dfdd784e60087209aa6"
 reuters_file.seek(0)
-import tarfile
 reuters_tar = tarfile.open(fileobj=reuters_file)
+print "Extracting", REUTERS_ARCHIVE
 reuters_tar.extractall("reuters21578")
