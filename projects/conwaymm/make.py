4436dec020cd1462cd98ba994f925e54304a7fac
anacrolix <anacrolix@localhost>
2009-08-03 04:47:47 +0000

    CONWAYMM: Branch from XP-VM WC.

---
+++ projects/conwaymm/make.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+
+from pybake import *
+
+CFLAGS = []
+LDFLAGS = []
+
+if os.name == 'nt':
+    import pybake.lang.cxx.msvc as cxx
+    CFLAGS += cxx.define("BOOST_ALL_DYN_LINK")
+    CFLAGS += cxx.include(r"C:\Boost\include\boost-1_38", r"win32/include")
+    LDFLAGS += cxx.libpath(r"C:\Boost\lib")
+    LDFLAGS += cxx.library("win32/lib/SDL", "win32/lib/SDLmain")
+else:
+    import pybake.lang.cxx.gcc as cxx
+    sdl_config = LibraryConfig("sdl-config")
+    CFLAGS = sdl_config(["--cflags"])
+    LDFLAGS = sdl_config(["--libs"]) + cxx.library("boost_thread-mt")
+
+cxx.executable("conway", ["main.cpp"], CFLAGS, LDFLAGS)
+
+pybake_main()
