048027a23b4411d43e04dd82208419f7677849c0
anacrolix <anacrolix@localhost>
2009-08-26 11:59:07 +0000

    ENQOUNTER: Some additional slots, and implementation.

--- staging/enqounter/mainwindow.cpp
+++ staging/enqounter/mainwindow.cpp
@@ -21,3 +21,37 @@ void MainWindow::addCombatant()
     b << "+0";
     ui->cmbtList->addTopLevelItem(new QTreeWidgetItem(b));
 }
+
+typedef QList<QTreeWidgetItem *> ListType;
+static void list_op(QList<QTreeWidgetItem *> list, void (*opfunc)(QTreeWidgetItem *))
+{
+    for (ListType::iterator i(list.begin()); i != list.end(); ++i)
+    {
+        opfunc(*i);
+    }
+}
+
+static void incrementInitiative(ListType::value_type item)
+{
+    item->setText(1, QString::number(item->text(1).toInt() + 1));
+}
+
+void MainWindow::incrementInitiative()
+{
+    list_op(ui->cmbtList->selectedItems(), ::incrementInitiative);
+}
+
+void MainWindow::restartEncounter()
+{
+    QList<QTreeWidgetItem *> list(ui->cmbtList->selectedItems());
+}
+
+void MainWindow::decrementInitiative()
+{
+    QList<QTreeWidgetItem *> list(ui->cmbtList->selectedItems());
+}
+
+void MainWindow::removeCombatant()
+{
+    QList<QTreeWidgetItem *> list(ui->cmbtList->selectedItems());
+}
