19a093fe3968ddd18f81c9916dae14339fd6136c
Matt Joiner <anacrolix@gmail.com>
2009-12-09 09:41:19 +1100

    pybake2: Add coloration, clean up some globals and workings for Windows.

--- algorithms/benchmark/pybake2.py
+++ algorithms/benchmark/pybake2.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import errno, os, pdb, threading, subprocess, sys
+from os import path
 
 class FatalThread(threading.Thread):
     def __init__(self, *args, **kwargs):
 	threading.Thread.__init__(self, *args, **kwargs)
+	assert not hasattr(self, "unhandled")
 	self.unhandled = False
     def run(self):
 	try:
@@ -22,23 +24,75 @@ class ThreadPool(object):
 	self.__threads[-1].start()
     def join_all(self):
 	for t in self.__threads:
+	    t.join()
 	    if t.unhandled:
 		sys.exit("wees & poos")
 
-#class FatalThread(threading.Thread):
-    #def run(self):
-	#try:
-	    #super(self.__class__, self).run()
-	#except:
-	    #sys.excepthook(*sys.exc_info())
-	    #sys.exit()
+def make_file_path(filepath):
+    try:
+	os.makedirs(path.split(filepath)[0])
+    except OSError as e:
+	if e.errno not in [errno.EEXIST]:
+	    raise
 
-#threading.Thread = FatalThread
-
-__jobSemaphore = threading.BoundedSemaphore(4)
+__jobSemaphore = threading.BoundedSemaphore(1)
 __targets = {}
 __phonies = {}
 _printLock = threading.Lock()
+
+
+
+try:
+    import curses
+except ImportError:
+    import ctypes
+    from ctypes import wintypes
+    class COORD(ctypes.Structure):
+	_fields_ = [
+		("X", wintypes.SHORT),
+		("Y", wintypes.SHORT),]
+    class CONSOLE_SCREEN_BUFFER_INFO(ctypes.Structure):
+	_fields_ = [
+		("dwSize", COORD),
+		("dwCursorPosition", COORD),
+		("wAttributes", wintypes.WORD),
+		("srWindow", wintypes.SMALL_RECT),
+		("dwMaximumWindowSize", COORD),]
+    def pb_print(*text, **kwargs):
+	sep = kwargs.get("sep", " ")
+	color = kwargs.get("color", None)
+	with _printLock:
+	    if color != None:
+		outHandle = ctypes.windll.kernel32.GetStdHandle(wintypes.DWORD(-11))
+		assert not outHandle == ctypes.wintypes.DWORD(-1)
+		a = CONSOLE_SCREEN_BUFFER_INFO()
+		assert ctypes.windll.kernel32.GetConsoleScreenBufferInfo(outHandle, ctypes.pointer(a))
+		oldAttributes = a.wAttributes
+		FOREGROUND_BLUE      = 0x0001
+		FOREGROUND_GREEN     = 0x0002
+		FOREGROUND_RED       = 0x0004
+		FOREGROUND_INTENSITY = 0x0008
+		BACKGROUND_BLUE      = 0x0010
+		BACKGROUND_GREEN     = 0x0020
+		BACKGROUND_RED       = 0x0040
+		BACKGROUND_INTENSITY = 0x0080
+		BACKGROUND_MASK      = 0xf0
+		codes = {
+			"blue": FOREGROUND_BLUE,
+			"cyan": FOREGROUND_BLUE | FOREGROUND_GREEN,
+			"green": FOREGROUND_GREEN,
+			"red": FOREGROUND_RED,
+			"yellow": FOREGROUND_GREEN | FOREGROUND_RED,}
+		code = codes[color]
+		code |= FOREGROUND_INTENSITY
+		code |= oldAttributes & BACKGROUND_MASK
+		ctypes.windll.kernel32.SetConsoleTextAttribute(outHandle, wintypes.WORD(code))
+	    try:
+		print sep.join(text).rstrip()
+	    finally:
+		if color != None:
+		    ctypes.windll.kernel32.SetConsoleTextAttribute(outHandle, oldAttributes)
+
 class UpdateGuard(object):
     def __init__(self):
 	self.__lock = threading.Lock()
@@ -68,13 +122,20 @@ __guard = UpdateGuard()
 class PybakeError(Exception):
     pass
 
+class PybakeStop(Exception):
+    pass
+
 def add_rule(outputs, inputs, commands):
     rule = (set(outputs), set(inputs), commands)
     for index, seq in enumerate([outputs, inputs, commands]):
-	assert len(rule[index]) == len(seq)
+	assert len(rule[index]) == len(seq), str(seq) + str(rule[index]) + str(index)
     for o in outputs:
         assert not o in __targets
         __targets[o] = (set(outputs), set(inputs), commands)
+
+def add_rules(rules):
+    for r in rules:
+	add_rule(*r)
 
 def is_outdated(target, deps):
     #print "is_outdated(", target, deps, ")"
@@ -85,7 +146,7 @@ def is_outdated(target, deps):
             return True
         else:
             raise
-    for d in deps | set([sys.argv[0]]):
+    for d in deps | set([opts.file]):
         if os.stat(d).st_mtime > targetMtime:
             return True
     else:
@@ -98,9 +159,11 @@ class ShellCommand(Command):
     def __init__(self, args):
         self.args = args
     def __call__(self):
-	with _printLock:
-	    print subprocess.list2cmdline(self.args)
-        subprocess.check_call(self.args)
+	pb_print(subprocess.list2cmdline(self.args), color="blue")
+        child = subprocess.Popen(self.args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	outdata, errdata = child.communicate()
+	if errdata: pb_print(errdata, color="red")
+	if outdata: pb_print(outdata, color="yellow")
 
 def update(target):
     outputs, inputs, commands = __targets[target]
@@ -110,16 +173,14 @@ def update(target):
     for i in inputs:
         if i in __targets:
 	    tp.add_thread(target=update, name=i, args=[i])
-	    #ts.append(threading.Thread(target=update, name=i, args=[i]))
-	    #ts[-1].start()
-    #for t in ts:
-    #    t.join()
     tp.join_all()
     __guard.wait_updated(set(inputs).intersection(__targets.keys()))
     if not is_outdated(target, inputs):
 	__guard.updated(set([target]))
         return
     with __jobSemaphore:
+	for o in outputs:
+	    make_file_path(o)
         for c in commands:
             c()
     for o in outputs:
@@ -206,6 +267,7 @@ class Project(object):
 
 class CProject(Project):
     __fields__ = [
+	"builddir",
 	"debug",
         "sources",
         "compiler",
@@ -221,6 +283,7 @@ class CProject(Project):
 	"libdirs",]
     def __init__(self, targetname):
 	super(CProject, self).__init__(targetname)
+	self.builddir = path.join(".pybake2", CONFIG)
 	self.debug = False
 	self.objects = []
 	self.targetdir = "build"
@@ -229,17 +292,11 @@ class CProject(Project):
 	#self.targetext = ""
 	self.linkoptions = []
     def generate_rules(self):
-	try:
-	    os.makedirs(self.targetdir)
-	except OSError as e:
-	    if e.errno not in [errno.EEXIST]:
-		raise
 	objs = self.objects
-	for s in self.sources:
-	    add_rule(*self.source_dep(s))
-	    object, rule = self.object(s)
-	    add_rule(*rule)
-	    objs.append(object)
+	for srcpath in self.sources:
+	    objpath, rules = self.compile(srcpath)
+	    add_rules(rules)
+	    objs.append(objpath)
 	targets, rule = self.targets(objs)
 	add_rule(*rule)
 	return targets
@@ -252,25 +309,13 @@ class CxxProject(CProject):
 	    return MsvcProject.__new__(MsvcProject, *args)
 	else:
 	    return GxxProject.__new__(GxxProject, *args)
-	        #def msvc_source_depgen(args, output):
-        #def inner():
-            #child = subprocess.Popen(args, stdout=subprocess.PIPE)
-            #DEP_PREFIX = "Note: including file:"
-            #retval = []
-            #with open(output, "wb") as dfp:
-                #for line in child.stdout:
-                    #if line.startswith(DEP_PREFIX):
-                        #dfp.write(line[len(DEP_PREFIX):].lstrip())
-            #assert child.wait() == 0
-        #return inner
-        #c = msvc_source_depgen(["cl", "/showIncludes", "/c", "/nologo", s, "/I" + "../../../gtest-trunk/include", "/I" + r"C:\Boost\include\boost-1_40", "/I" + r"C:\Python26\include"], o)
+
 class GenerateMsvcSourceDeps(Command):
     def __init__(self, cmdargs, outpath):
 	self.cmdargs = cmdargs
 	self.outpath = outpath
     def __call__(self):
-	with _printLock:
-	    print subprocess.list2cmdline(self.cmdargs)
+	pb_print(subprocess.list2cmdline(self.cmdargs), color="blue")
 	child = subprocess.Popen(self.cmdargs, stdout=subprocess.PIPE)
 	DEP_PREFIX = "Note: including file:"
 	hdrdeps = set([])
@@ -286,24 +331,30 @@ def parse_source_dep_file(depFilePath):
     except IOError as e:
 	if e.errno not in [errno.ENOENT]:
 	    raise
-    except SyntaxError:
-	pass
+    #except SyntaxError:
+	#pass
     return []
 
 class MsvcProject(CxxProject):
     def __init__(self, targetname):
 	super(self.__class__, self).__init__(targetname)
 	self.targetext = ".exe"
-    def source_dep(self, source):
-	o = source + ".d"
-	return [o], [source], [GenerateMsvcSourceDeps(["cl", source, "/showIncludes", "/c", "/nologo"] + ["/I" + id for id in self.includedirs], o)]
-    def object(self, source):
-	inputs = parse_source_dep_file(source + ".d") + [source]
-	object = source + ".o"
-	args = ["cl", "/Fo" + object, source, "/c", "/nologo", "/EHsc", "/MD"] + ["/I" + id for id in self.includedirs]
+    def source_dep(self, srcpath, hdeppath):
+	return [hdeppath], [srcpath],
+    def compile(self, srcpath):
+	hdeppath = path.join(self.builddir, srcpath + ".d")
+	objpath = path.join(self.builddir, srcpath + ".obj")
+	hdeprule = (
+		[hdeppath],
+		[srcpath], [GenerateMsvcSourceDeps(
+			["cl", srcpath, "/showIncludes", "/c", "/nologo"] + ["/I" + id for id in self.includedirs],
+			hdeppath)])
+	args = ["cl", "/Fo" + objpath, srcpath, "/c", "/nologo", "/EHsc", "/MD"] + ["/I" + id for id in self.includedirs]
 	if self.debug:
-	    raise PybakeError("Debug not supported for MSVC")
-	return object, ([object], inputs, [ShellCommand(args)])
+	    args.append("/Zi")
+	inputs = parse_source_dep_file(hdeppath) + [srcpath]
+	objrule = [objpath], inputs, [ShellCommand(args)]
+	return objpath, [hdeprule, objrule]
     def targets(self, objects):
 	target = os.path.join(self.targetdir, self.targetprefix + self.targetname + self.targetext)
 	#print objects
@@ -315,8 +366,7 @@ class GenerateMakeSourceDeps(Command):
 	self.outpath = outpath
 	self.target = target
     def __call__(self):
-	with _printLock:
-	    print subprocess.list2cmdline(self.cmdargs)
+	pb_print(subprocess.list2cmdline(self.cmdargs), color="blue")
 	child = subprocess.Popen(self.cmdargs, stdout=subprocess.PIPE)
 	targ, deps = parse_make_rule(child.communicate()[0])
 	assert targ == [self.target]
@@ -350,13 +400,18 @@ def add_project(project):
     assert not project in __projects
     __projects.add(project)
 
-def set_configurations(*confs):
-    global __configurations
-    __configurations = tuple(confs)
+def set_configs(*confs):
+    global __configs
+    global CONFIG
+    __configs = tuple(confs)
+    if CONFIG == None:
+	CONFIG = __configs[0]
+    elif CONFIG not in __configs:
+	raise PybakeError('Config "%s" is not in recipe' % (CONFIG,))
 
-def is_configuration(conf):
-    assert conf in __configurations
-    return CONFIGURATION == conf
+def is_config(conf):
+    assert conf in __configs
+    return CONFIG == conf
 
 from optparse import OptionParser
 parser = OptionParser()
@@ -365,10 +420,10 @@ parser.add_option("--config")
 parser.add_option("--file")
 opts, args = parser.parse_args()
 
-CONFIGURATION = opts.config
+CONFIG = opts.config
 execfile(opts.file, globals(), {})
 
-print CONFIGURATION
+print CONFIG
 for p in __projects:
     assert not p.name in __phonies
     __phonies[p.name] = p.generate_rules()
