81301e3adde759ebe956063f13479daecf0bb983
Matt Joiner <anacrolix@gmail.com>
2010-01-04 00:44:37 +1100

    Improve ShellCommand error reporting and structure. Improve efficiency of MSVC header dep generation. Handle special target kinds on MSVC.

--- utility/_pybake2/project.py
+++ utility/_pybake2/project.py
@@ -21,11 +21,14 @@ class ShellCommand(Command):
 	except OSError as e:
 	    if e.errno != errno.ENOENT:
 		raise
-	    raise PybakeStop("A necessary file was not found")
-	self.process_streams(*child.communicate())
-	if child.returncode != 0:
+	    raise PybakeStop("Executable %s could not be located" % repr(self.args[0]))
+	finally:
+	    pass
+	self.interact(child)
+	if child.wait() != 0:
 	    raise PybakeStop("Command returned %d: %s" % (child.returncode, repr(cmdline)))
-    def process_streams(self, outdata, errdata):
+    def interact(self, process):
+	outdata, errdata = process.communicate()
 	if errdata: pb_print(errdata, color="red")
 	if outdata: pb_print(outdata, color="yellow")
 
@@ -62,29 +65,37 @@ class CProject(Project):
     __fields__ = [
 	    "builddir",
 	    "buildoptions",
+	    #"compiler",
 	    "debug",
-	    "sources",
-	    "compiler",
+	    "defines",
+	    "kind",
 	    "linkoptions",
 	    "links",
 	    "libdirs",
 	    "includedirs",
 	    "objects",
 	    "optimize",
+	    "sources",
 	    "targetdir",
-	    "targetprefix",
-	    "targetname",
 	    "targetext",
+	    "targetname",
+	    "targetprefix",
 	]
+    @property
+    def target(self):
+	raise NotImplementedError
     def __init__(self, targetname, recipe):
 	super(CProject, self).__init__(targetname, recipe)
-	self.builddir = path.join(".pybake2", sys.platform, recipe.get_config())
+	self.builddir = path.join(".pybake2", self.name, sys.platform, recipe.get_config())
 	self.debug = False
+	self.defines = []
 	self.includedirs = []
+	self.kind = "exe"
 	self.libdirs = []
 	self.links = []
 	self.objects = []
 	self.optimize = False
+	self.sources = []
 	self.targetdir = ""
 	self.targetprefix = ""
 	self.targetname = targetname
@@ -110,24 +121,6 @@ class CxxProject(CProject):
 	else:
 	    return GxxProject.__new__(GxxProject, *args)
 
-class GenerateMsvcSourceDeps(Command):
-    def __init__(self, cmdargs, outpath, srcpath):
-	self.cmdargs = cmdargs
-	self.outpath = outpath
-	self.srcpath = srcpath
-    def __call__(self):
-	pb_print(subprocess.list2cmdline(self.cmdargs), color=CMDLINE)
-	child = subprocess.Popen(self.cmdargs, stdout=subprocess.PIPE)
-	DEP_PREFIX = "Note: including file:"
-	hdrdeps = set([])
-	for line in child.stdout:
-	    if line.startswith(DEP_PREFIX):
-		hdrdeps.add(line[len(DEP_PREFIX):].strip())
-	    elif line.rstrip() != path.basename(self.srcpath) and len(line.strip()):
-		pb_print(line, color=WARNING)
-	assert child.wait() == 0
-	open(self.outpath, "w").write(repr(hdrdeps))
-
 def parse_source_dep_file(depFilePath):
     try:
 	return list(eval(open(depFilePath).read()))
@@ -139,40 +132,81 @@ def parse_source_dep_file(depFilePath):
     return []
 
 class MsvcCompileCommand(ShellCommand):
-    def process_streams(self, outdata, errdata):
+    def __init__(self, args, hdeppath, srcpath):
+	super(self.__class__, self).__init__(args)
+	self.hdeppath = hdeppath
+	self.srcpath = srcpath
+    def interact(self, process):
+	DEP_PREFIX = "Note: including file:"
+	hdrdeps = set([])
+	for line in process.stdout:
+	    if line.startswith(DEP_PREFIX):
+		hdrdeps.add(line[len(DEP_PREFIX):].strip())
+	    elif line.rstrip() != path.basename(self.srcpath) and len(line.strip()) > 0:
+		pb_print(line, color=WARNING)
+	outdata, errdata = process.communicate()
+	assert len(outdata) == 0
 	if errdata: pb_print(errdata, color="red")
-	if outdata: pb_print(outdata.rstrip(), color="yellow")
+	open(self.hdeppath, "w").write(repr(hdrdeps))
 
 class MsvcProject(CxxProject):
     def __init__(self, targetname, recipe):
 	super(self.__class__, self).__init__(targetname, recipe)
-	self.targetext = ".exe"
+	self.targetext = None
     def source_dep(self, srcpath, hdeppath):
 	return [hdeppath], [srcpath],
     def compile(self, srcpath):
 	hdeppath = path.join(self.builddir, srcpath + ".d")
 	objpath = path.join(self.builddir, srcpath + ".obj")
-	hdeprule = (
-		[hdeppath],
-		[srcpath],
-		[GenerateMsvcSourceDeps(
-			["cl", srcpath, "/showIncludes", "/c", "/nologo", "/EHsc"] + ["/I" + id for id in self.includedirs],
-			hdeppath,
-			srcpath)])
-	args = ["cl", "/Fo" + objpath, srcpath, "/c", "/nologo", "/EHsc", "/MD"] + ["/I" + id for id in self.includedirs]
+	args = ["cl", "/nologo", "/EHsc", "/c", "/showIncludes"]
 	if self.debug:
-	    args.append("/Zi")
+	    args += ["/Zi", "/MDd"]
+	else:
+	    args += ["/MD"]
 	if self.optimize:
-	    args.append("/O2")
-	inputs = parse_source_dep_file(hdeppath) + [srcpath]
-	objrule = [objpath], inputs, [MsvcCompileCommand(args)]
-	return objpath, [hdeprule, objrule]
+	    args.append("/Ox")
+	args += [srcpath]
+	args += ["/Fo" + objpath]
+	args += ["/I" + id for id in self.includedirs]
+	defines = self.defines
+	if self.kind == "dll":
+	    defines += ["/D_WINDLL"]
+	args += ["/D" + d for d in defines]
+	rule = (
+		[hdeppath, objpath],
+		[srcpath] + parse_source_dep_file(hdeppath),
+		[MsvcCompileCommand(args, hdeppath, srcpath)])
+	return objpath, [rule]
+    @property
+    def target(self):
+	if self.targetext == None:
+	    self.targetext = {
+		    'exe': ".exe",
+		    'dll': ".dll",
+		    'lib': ".lib",
+		}[self.kind]
+	return os.path.join(self.targetdir, self.targetprefix + self.targetname + self.targetext)
     def targets(self, objects):
-	target = os.path.join(self.targetdir, self.targetprefix + self.targetname + self.targetext)
-	#print objects
-	return target, ([target], objects, [ShellCommand(["link", "/OUT:" + target] + objects + ["/LIBPATH:" + lp for lp in self.libdirs] + self.links + ["/NOLOGO"])])
+	if self.kind == 'lib':
+	    args = ["lib"]
+	else:
+	    args = ["link"]
+	    if self.debug:
+		args.append("/DEBUG")
+	    if self.kind == 'dll':
+		args += ["/DLL"]
+	args += ["/OUT:" + self.target] + ["/NOLOGO"]
+	args += ["/LIBPATH:" + lp for lp in self.libdirs]
+	intradeps = []
+	for l in self.links:
+	    if isinstance(l, CProject):
+		l = l.target
+		intradeps.append(l)
+	    args.append(l)
+	args += objects
+	return self.target, ([self.target], objects + intradeps, [ShellCommand(args)])
 
-class GenerateGccSourceDeps(Command):
+class GenerateGccSourceDeps(ShellCommand):
     def __init__(self, cmdargs, outpath, srcpath, dummyTarget):
 	self.cmdargs = cmdargs
 	self.outpath = outpath
