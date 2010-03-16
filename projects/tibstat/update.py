fcc83a153a00967441253e3a5fdcafc8f7b422df
Matt Joiner <anacrolix@gmail.com>
2010-03-16 13:07:02 +1100

    Tibstat improvements Try to improve the multiprocess sqlite3 database locking issue. Handle character former names. Move database update functions into dbiface. Handle character does not exist case in tibiacom. Add function to update characters th haven't had their character page parsed.

--- projects/tibstat/update.py
+++ projects/tibstat/update.py
@@ -1,43 +1,10 @@
 #!/usr/bin/env python
 
-import calendar, logging, pdb, pprint, Queue, sys, threading, time, urllib2
+import calendar, logging, pdb, pprint, Queue, sys, threading, time, traceback, urllib2
 
 import dbiface, tibiacom
+from dbiface import TibstatDatabase
 from tibiacom import next_whoisonline_update as next_tibiacom_whoisonline_update
-
-class TibstatDatabase(dbiface.TibstatDatabase):
-
-    def update_guilds(self, world):
-        for guild in tibiacom.get_world_guilds(world):
-            print >>sys.stderr, "Updating guild", guild
-            for member in tibiacom.get_guild_members(guild):
-                dbiface.update_char(member, guild=guild, world=world)
-
-    def update_world_online(self, world):
-        html, headers = tibiacom.http_get(tibiacom.world_online_url(world))
-        players = tibiacom.parse_world_online(html)
-        logging.info("Found %d players on %s", len(players), world)
-        serverStamp = dbiface.to_unixepoch(headers["Date"])
-        assert abs(int(time.time()) - serverStamp) < 30
-        print dbiface.to_unixepoch(headers["Date"]), int(time.time())
-        for p in players:
-            self._update_char(
-                    p.name, level=p.level, vocation=p.vocation, online=True,
-                    stamp=serverStamp, world=world)
-        #self.commit()
-
-    def update_char(self, name):
-        info = tibiacom.get_char_page(name)
-        self._update_char(info.pop("name"), **info)
-
-    def update_recent_deaths(self, after):
-        for row in dbiface.get_online_chars(after):
-            update_char(row["name"])
-
-_tsdb = TibstatDatabase()
-for a in ("update_world_online", "update_char"):
-    assert not hasattr(sys.modules[__name__], a)
-    setattr(sys.modules[__name__], a, getattr(_tsdb, a))
 
 def standard_world_update(world, *options):
     nextUpdate = next_tibiacom_whoisonline_update()
@@ -64,20 +31,28 @@ def update_world_list():
     dbiface.set_worlds(tibiacom.tibia_worlds())
 
 def _continuous_update_thread_func(jobqueue, errflag):
-    tsdb = TibstatDatabase()
-    while not errflag.is_set():
-        try:
+    try:
+        tsdb = TibstatDatabase()
+        while not errflag.is_set():
             job = jobqueue.get()
-            #print job
-            if job is None:
-                return
-            job[0](tsdb, *job[1:])
-            tsdb.commit()
-        except:
-            errflag = True
-            raise
-        finally:
-            jobqueue.task_done()
+            try:
+                #print job
+                if job is None:
+                    return
+                job[0](tsdb, *job[1:])
+                #tsdb.commit()
+            finally:
+                jobqueue.task_done()
+    except:
+        errflag.set()
+        while not jobqueue.empty():
+            try:
+                jobqueue.get_nowait()
+            except Queue.Empty:
+                break
+        raise
+    finally:
+        print "thread quit", errflag.is_set()
 
 def continuous_update(*worlds):
 
@@ -120,6 +95,8 @@ def continuous_update(*worlds):
             for w in worlds:
                 jobqueue.put((TibstatDatabase.update_world_online, w))
             jobqueue.join()
+            if errflag.is_set():
+                return
 
             # check the time hasn't gone over into the next update period
             if not immediate:
@@ -127,13 +104,15 @@ def continuous_update(*worlds):
 
             charCount = 0
             for w in worlds:
-                for row in dbiface.get_online_chars(
+                for row in TibstatDatabase().get_online_chars(
                         after=(nextUpdate - 10 * 60),
                         world=w):
                     jobqueue.put((TibstatDatabase.update_char, row["name"]))
                     charCount += 1
             logging.info("Updating %d chars", charCount)
             jobqueue.join()
+            if errflag.is_set():
+                return
 
             logging.info("Update took %ds", int(time.time()) - startTime)
             if once:
@@ -145,7 +124,33 @@ def continuous_update(*worlds):
             jobqueue.put(None)
         for a in threads:
             a.join()
-        jobqueue.join()
+        #jobqueue.join()
+
+def _char_incomplete(charRow):
+    for field in charRow.keys():
+        if field not in ("guild", "created", "deletion", "former_names"):
+            if charRow[field] is None:
+                return True
+    else:
+        return False
+
+def update_incomplete_chars(world=None):
+    for char in dbiface.get_all_chars(world):
+        #print char
+        charName = char["name"]
+        assert charName
+        if _char_incomplete(char):
+            logging.info("Updating %r", charName)
+            try:
+                newCharName = dbiface.update_char(charName)
+            except tibiacom.CharDoesNotExist as e:
+                assert e.args[0] == charName
+                logging.warning("Character %r does not exist", charName)
+            else:
+                newCharRow = dbiface.get_char(newCharName)
+                #print newCharRow
+                assert not _char_incomplete(newCharRow), newCharRow
+                #dbiface.commit()
 
 def main():
     logging.basicConfig(
@@ -153,7 +158,6 @@ def main():
             stream=sys.stdout,
             format="%(levelname)s:%(process)d:%(message)s")
     pprint.pprint(globals()[sys.argv[1]](*sys.argv[2:]))
-    _tsdb.commit()
 
 if __name__ == "__main__":
     main()
