74244830bbb6d49c01ac00a6941f639a34e48f7b
Matt Joiner <anacrolix@gmail.com>
2010-05-31 18:18:09 +1000

    Add AFK version of telas_golem_training. Don't notify if hitpoints are rising. Notify of skill progression. Move safe list into a new module.

---
+++ tibia/2010/eruanno.py
@@ -0,0 +1,3 @@
+SAFE_LIST = ("Plashz", "Orelius", "Royal Makia", "Cuppydo", \
+		"Kataztros", "Blackarcher", "Zaus Tibard", "Mariononarcizo", \
+		"Lezalion", "Tuffandgong", "Omarshino", "Poison Gurl")
