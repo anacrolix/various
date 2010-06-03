0d5509dbefd7363a4c7f6b32bf086ce40f5ba091
Matt Joiner <anacrolix@gmail.com>
2010-06-03 20:55:38 +1000

    2010 tibia linux bot: Block signals while waiting for tracee to stop after ptrace attach.

--- tibia/2010/readmem.c
+++ tibia/2010/readmem.c
@@ -1,7 +1,13 @@
 #include "botutil.h"
 
+#define BLOCK_SIGNALS 1
+
 /*
-Returns 0: success, 1: general failure, 2: argument issue, 3: retry
+Returns
+	0: success,
+	1: general failure,
+	2: argument issue,
+	3: attach failed, try again
 */
 int main(int argc, char **argv)
 {
@@ -32,10 +38,26 @@ int main(int argc, char **argv)
 
 	/* attach to target process */
 
-	// attach or exit with code 3 to indicate try again
+	// block all signals, we can't blow up while waiting for the child to stop
+	// or the child will freeze when it's SIGSTOP arrives and we don't clear it
+#if defined(BLOCK_SIGNALS)
+	sigset_t oldset;
+	{
+		sigset_t newset;
+		verify(0 == sigfillset(&newset));
+		// out of interest, we ensure the most likely signal is present
+		assert(1 == sigismember(&newset, SIGINT));
+		verify(0 == sigprocmask(SIG_BLOCK, &newset, &oldset));
+	}
+#endif
+
+	// attach or exit with code 3
 	if (0 != ptrace(PTRACE_ATTACH, pid, NULL, NULL))
 	{
 		int errattch = errno;
+		// if ptrace() gives EPERM, it might be because another process
+		// is already attached, there's no guarantee it's still attached by
+		// the time we check so this is a best attempt to determine who is
 		if (errattch == EPERM)
 		{
 			pid_t tracer = get_tracer_pid(pid);
@@ -45,10 +67,16 @@ int main(int argc, char **argv)
 				return 3;
 			}
 		}
-		error(3, errattch, "ptrace(PTRACE_ATTACH)");
+		error(errattch == EPERM ? 3 : 1, errattch, "ptrace(PTRACE_ATTACH)");
 	}
 
+	//verify(0 == raise(SIGINT));
+
 	wait_until_tracee_stops(pid);
+
+#if defined(BLOCK_SIGNALS)
+	verify(0 == sigprocmask(SIG_SETMASK, &oldset, NULL));
+#endif
 
 	int memfd = open(mempath, O_RDONLY);
 	assert(memfd != -1);
