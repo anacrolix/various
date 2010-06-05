8f261bc57e699b68d591bc6e63bfd9a043e55622
Matt Joiner <anacrolix@gmail.com>
2010-06-05 15:13:53 +1000

    Updates to Tibia data files.

--- tibia/2010/eruanno.py
+++ tibia/2010/eruanno.py
@@ -1,4 +1,4 @@
 SAFE_LIST = ("Plashz", "Orelius", "Royal Makia", "Cuppydo", \
 		"Kataztros", "Blackarcher", "Zaus Tibard", "Mariononarcizo", \
 		"Lezalion", "Tuffandgong", "Omarshino", "Poison Gurl", \
-		"Confu Jusko")
+		"Confu Jusko", "Mefherel Dehnovo")
