456c9491ec834bc9ba18bb36bc11b79a2548a9da
Matt Joiner <anacrolix@gmail.com>
2010-06-09 20:08:31 +1000

    Use signals to trigger activity, this makes the script useful for testing tracers.

--- tibia/butox/stopme.py
+++ tibia/butox/stopme.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
-import itertools, math, time
+#import itertools, math, time
 
+#while True:
+	#slptime = 1 - math.modf(time.time())[0]
+	#if slptime >= 1:
+		#slptime = 0
+	#time.sleep(slptime)
+	#print int(time.time())
+
+def handler(signum, frame):
+	from time import ctime
+	print ctime()
+	assert 0 == alarm(1)
+
+from signal import alarm, pause, signal, SIGALRM
+signal(SIGALRM, handler)
+assert 0 == alarm(1)
 while True:
-	slptime = 1 - math.modf(time.time())[0]
-	if slptime >= 1:
-		slptime = 0
-	time.sleep(slptime)
-	print int(time.time())
+	pause()
