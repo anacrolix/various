401899679279637aabfd13a7e446421365a2787b
Matt Joiner <anacrolix@gmail.com>
2010-07-05 19:06:33 +1000

    lanshare: Major GUI improvements Add buttons for peer page, start working on invoking FTP browsers for each platform. Add a UDP broadcast peer socket, that detects other clients and lets you invoke their shares. Remove bogus "--no-gui" option.

--- projects/lanshare/lanshare-gtk.py
+++ projects/lanshare/lanshare-gtk.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
-import gtk
-from lanshare import Lanshare
 import pdb
+import time
+
+import glib
+import gtk
+
+from lanshare import Lanshare
 
 def stock_image(stock_id):
 	image = gtk.Image()
@@ -11,9 +15,13 @@ def stock_image(stock_id):
 
 class LanshareGtk(Lanshare):
 
-	def __init__(self):
-		Lanshare.__init__(self)
+	# remove peer if announce isn't received for this long
+	peer_timeout = 3
+	# send out announces and check timeouts with this interval
+	peer_announce_interval = 1
 
+	def create_gui(self):
+		# store contains (virtual dir name, actual fs path)
 		sharestore = gtk.ListStore(str, str)
 
 		shareview = gtk.TreeView(sharestore)
@@ -21,6 +29,7 @@ class LanshareGtk(Lanshare):
 				-1, "Share Name", gtk.CellRendererText(), text=0)
 		shareview.insert_column_with_attributes(
 				-1, "Local Path", gtk.CellRendererText(), text=1)
+		shareview.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
 
 		addshrbtn = gtk.Button("New Share", stock=gtk.STOCK_ADD)
 		addshrbtn.connect("clicked", self.add_share)
@@ -37,19 +46,41 @@ class LanshareGtk(Lanshare):
 		sharepage.add(shareview)
 		sharepage.pack_start(sharebtns, expand=False)
 
-		peerstore = gtk.ListStore(str)
+		# hostname, host, serv, lasttime, protocol
+		peerstore = gtk.ListStore(str, str, int, float, str)
+		# could do this manually and wrap a dict of this format:
+		#	{(hostname, host, serv, protocol): lasttime}
+		#peerstore = gtk.GenericTreeModel()
 
 		peerview = gtk.TreeView(peerstore)
 		peerview.insert_column_with_attributes(
-				-1, "Peer Name", gtk.CellRendererText())
-		peerview.insert_column_with_attributes(
-				 -1, "Network Address", gtk.CellRendererText(), text=0)
+				-1, "Peer Name", gtk.CellRendererText(), text=0)
+		peerview.insert_column_with_data_func(
+				 -1, "Network Address", gtk.CellRendererText(),
+				 self.peer_network_address_cell_data_func)
+		peerview.connect("row-activated", self.peer_view_row_activated)
+
+		browse_peer_button = gtk.Button("Browse")
+		browse_peer_button.set_image(stock_image(gtk.STOCK_HARDDISK))
+		browse_peer_button.connect("clicked", self.browse_peer_button_clicked, peerview)
+
+		refresh_peers_button = gtk.Button("Refresh")
+		refresh_peers_button.set_image(stock_image(gtk.STOCK_FIND))
+
+		peer_buttons = gtk.VButtonBox()
+		peer_buttons.add(browse_peer_button)
+		peer_buttons.add(refresh_peers_button)
+		peer_buttons.set_layout(gtk.BUTTONBOX_START)
+
+		peers_page = gtk.HBox()
+		peers_page.pack_start(peerview)
+		peers_page.pack_start(peer_buttons, expand=False)
 
 		notebook = gtk.Notebook()
 		notebook.append_page(sharepage)
 		notebook.set_tab_label_text(sharepage, "Shares")
-		notebook.append_page(peerview)
-		notebook.set_tab_label_text(peerview, "Peers")
+		notebook.append_page(peers_page)
+		notebook.set_tab_label_text(peers_page, "Peers")
 
 		window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		window.connect("destroy", self.destroy)
@@ -57,13 +88,112 @@ class LanshareGtk(Lanshare):
 		window.set_title(self.__title__)
 		window.show_all()
 
-		from gobject import idle_add
-		idle_add(self.gui_idle)
-
+		# save these for later manipulation
+		self.peerstore = peerstore
 		self.sharestore = sharestore
 		self.shareview = shareview
 
+	def browse_peer(self, url):
+		import subprocess
+		try:
+			subprocess.check_call(["xdg-open", url])
+		except subprocess.CalledProcessError as exc:
+			print exc
+		try:
+			subprocess.check_call(["nautilus", url])
+		finally:
+			pass
+		#except Exception as exc:
+		#	pdb.set_trace()
+
+	def peer_view_row_activated(self, view, path, column):
+		self.browse_peer(self.get_peer_url(view.get_model()[path]))
+
+	def browse_peer_button_clicked(self, button, view):
+		model, iter = view.get_selection().get_selected()
+		self.browse_peer(self.get_peer_url(model[iter]))
+
+	def get_peer_url(self, peer_store_row):
+		from urlparse import urlunsplit
+		return urlunsplit((
+				peer_store_row[4],
+				"{0}:{1}".format(peer_store_row[1], peer_store_row[2]),
+				"", "", ""))
+
+	def peer_network_address_cell_data_func(self, column, cell, model, iter):
+		text = self.get_peer_url(model[iter])
+		print text
+		cell.set_property("text", text)
+		#cell.text =
+
+	def create_peer_socket(self):
+		import socket
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
+		# TODO make this ipv4/6 transparent
+		sock.bind(("", self.port))
+
+		glib.io_add_watch(
+				sock,
+				glib.IO_IN,
+				self.peer_socket_event)
+
+		self.peersock = sock
+		#self.peeraddrs = set()
+
+		self.send_peer_announce()
+
+	def send_peer_announce(self):
+		from socket import gethostname
+		self.peersock.sendto(
+				repr({"protocol": "ftp", "hostname": gethostname()}),
+				("255.255.255.255", self.port))
+
+	def peer_socket_event(self, source, condition):
+		if condition & glib.IO_IN:
+			data, addr = self.peersock.recvfrom(0x100)
+			recvtime = time.time()
+			data = eval(data)
+			hostname = data["hostname"]
+			protocol = data["protocol"]
+			for row in self.peerstore:
+				# find matching row
+				if 		row[0] == hostname and \
+						row[1] == addr[0] and \
+						row[2] == addr[1] and \
+						row[4] == protocol:
+					# update the announce recv time
+					row[3] = recvtime
+					break
+			else:
+				# no peer match found, so add it
+				self.peerstore.append(
+						(hostname, addr[0], addr[1], recvtime, protocol))
+		return True
+
+	def __init__(self):
+		Lanshare.__init__(self)
+
+		self.create_gui()
+		self.create_peer_socket()
+
+		from glib import idle_add, timeout_add_seconds
+		idle_add(self.gui_idle)
+		timeout_add_seconds(self.peer_announce_interval, self.peer_tick)
+
 		self.populate_shares()
+
+	def peer_tick(self):
+		print "peer tick"
+		self.send_peer_announce()
+		from time import time
+		delrefs = []
+		for row in self.peerstore:
+			if time() >= row[3] + self.peer_timeout:
+				delrefs.append(gtk.TreeRowReference(row.model, row.path))
+		for ref in delrefs:
+			del self.peerstore[ref.get_path()]
+		return True
 
 	def populate_shares(self):
 		self.sharestore.clear()
@@ -116,13 +246,15 @@ class LanshareGtk(Lanshare):
 
 	def remove_share(self, widget=None):
 		store, rowpaths = self.shareview.get_selection().get_selected_rows()
-		for path in rowpaths:
+		# reverse the paths so we delete from the back, so as not to invalidate
+		# paths that come earlier
+		for path in reversed(rowpaths):
+			# the first column of the store contains the key
 			del self.config.shares[store[path][0]]
 		self.populate_shares()
 
 	def gui_idle(self):
 		try:
-			#print "boobies"
 			self.server.serve_forever(use_poll=True, count=1, timeout=0.02)
 			return True
 		except KeyboardInterrupt:
