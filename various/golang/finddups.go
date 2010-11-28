7418204c464b14e11d725bc918d245ed2ec76a61
Matt Joiner <anacrolix@gmail.com>
2010-11-29 00:05:22 +1100

    Wrote a few golang programs

---
+++ various/golang/finddups.go
@@ -0,0 +1,104 @@
+package main
+
+import "crypto/md5"
+import "flag"
+import "fmt"
+import "path"
+import "os"
+import "runtime"
+
+func abspath(p string) string {
+	base, _ := os.Getwd()
+	return path.Clean(path.Join(base, p))
+}
+
+type Walker struct {
+	paths   chan<- string
+	visited map[string]bool
+}
+
+func (me Walker) VisitDir(path string, f *os.FileInfo) bool {
+	path = abspath(path)
+	if !me.visited[path] {
+		me.visited[path] = true
+		return true
+	}
+	return false
+}
+
+func (me Walker) VisitFile(path string, f *os.FileInfo) {
+	abspath := abspath(path)
+	if !me.visited[abspath] {
+		me.visited[abspath] = true
+		me.paths <- path
+	}
+}
+
+func walkargs(args []string, paths chan<- string) {
+	defer close(paths)
+	errors := make(chan os.Error)
+	defer close(errors)
+	var manager Manager
+	walker := Walker{paths: paths, visited: make(map[string]bool)}
+	for _, arg := range args {
+		manager.Go(func(root string) func() { return func() {
+			fmt.Println(root)
+			path.Walk(root, walker, errors)
+		}}(arg))
+	}
+	go func() {
+		for {
+			err := <- errors
+			if closed(errors) {
+				return
+			}
+			fmt.Println(err)
+		}
+	}()
+	manager.Wait()
+}
+
+type Dupes map[string][]string
+
+func hashfile(name string, dupes Dupes) {
+	fmt.Println("Hashing file:", name)
+	file, err := os.Open(name, os.O_RDONLY, 0)
+	if file == nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	hash := md5.New()
+	var buf [0x1000]byte
+	for {
+		n, err := file.Read(buf[:])
+		//fmt.Println(name, n, err)
+		hash.Write(buf[:n])
+		if err == os.EOF {
+			break
+		} else if n == 0 {
+			fmt.Println(err)
+			return
+		}
+	}
+	sum := string(hash.Sum())
+	dupes[sum] = append(dupes[sum], name)
+	fmt.Printf("%x: %v\n", sum, name)
+}
+
+func main() {
+	fmt.Println("GOMAXPROCS:", runtime.GOMAXPROCS(8))
+	flag.Parse()
+	paths := make(chan string)
+	go walkargs(flag.Args(), paths)
+	dupes := make(Dupes)
+	var hashmgr Manager
+	for {
+		name := <-paths
+		if closed(paths) {
+			break
+		}
+		hashmgr.Go(func() { hashfile(name, dupes) })
+	}
+	hashmgr.Wait()
+}
---
+++ various/golang/gopool.go
@@ -0,0 +1,47 @@
+package dispatch
+
+import "sync"
+
+type Manager struct {
+	lock    sync.Mutex
+	running uint
+	waiting uint
+	wakeup  chan bool
+}
+
+func (m *Manager) Go(fn func()) {
+	m.lock.Lock()
+	m.running++
+	m.lock.Unlock()
+	go func() {
+		fn()
+		defer func() {
+			m.lock.Lock()
+			m.running--
+			if m.running == 0 && m.waiting > 0 {
+				for ; m.waiting > 0; m.waiting-- {
+					m.wakeup <- true
+				}
+				m.wakeup = nil
+			}
+			m.lock.Unlock()
+		}
+	}()
+}
+
+func (m *Manager) Wait() {
+	wait := false
+	m.lock.Lock()
+	if m.wakeup == nil {
+		m.wakeup = make(chan bool)
+	}
+	c := m.wakeup
+	if m.running > 0 {
+		m.waiting++
+		wait = true
+	}
+	m.lock.Unlock()
+	if wait {
+		<-c
+	}
+}
