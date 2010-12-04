ab9bcbeb82748aceb3645132ba79144dc31944c8
Matt Joiner <anacrolix@gmail.com>
2010-12-04 23:53:33 +1100

    Send pybake 1 and 2 away to pybake subrepo

--- utility/pybake/__init__.py
+++
@@ -1,9 +0,0 @@
-import buildsys
-import lang
-
-from classes import \
-    BuildStep, Configure, ExplicitRule, ImplicitRule, \
-    Install, LibraryConfig, PhonyRule, SystemTask
-from functions import pybake_main, clean_targets
-
-functions.initialize()
