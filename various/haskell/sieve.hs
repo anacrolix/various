fcd5f2daf475592d4b31e9797b6f9ff1e359419e
Matt Joiner <anacrolix@gmail.com>
2011-11-04 21:08:25 +1100

    Haskell prime sieve: Send Nothing on channels when routine completes

--- various/haskell/sieve.hs
+++ various/haskell/sieve.hs
@@ -5,23 +5,31 @@ import Control.Monad
 import Control.Concurrent.Chan
 import Control.Concurrent
 
-filter in_ out prime = forever $ do
-    n <- readChan in_
-    case n `mod` prime of
-        0 -> return ()
-        _ -> writeChan out n
+filter in_ out prime = do
+    value <- readChan in_
+    case value of
+        Nothing -> writeChan out Nothing
+        Just n -> do
+            case n `mod` prime of
+                0 -> return ()
+                _ -> writeChan out (Just n)
+            filter in_ out prime
 
 generate ch max = do
-    mapM_ (writeChan ch) $ enumFromTo 2 max
+    mapM_ (writeChan ch . Just) $ enumFromTo 2 max
+    writeChan ch Nothing
 
 sieve ch = do
-    prime <- readChan ch
-    print prime
-    ch1 <- newChan
-    forkIO $ filter ch ch1 prime
-    sieve ch1
+    value <- readChan ch
+    case value of
+        Nothing -> return ()
+        Just prime -> do
+            print prime
+            ch1 <- newChan
+            forkIO $ filter ch ch1 prime
+            sieve ch1
 
 main = do
     ch <- newChan
-    forkIO $ generate ch 10000
+    forkIO $ generate ch 100
     sieve ch
