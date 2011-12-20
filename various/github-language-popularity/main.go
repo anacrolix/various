62f75619dbc78ddac030d859d1c8af2daec54adc
Matt Joiner <anacrolix@gmail.com>
2011-12-20 21:36:55 +1100

    Remove debugging code, and source material

--- various/github-language-popularity/main.go
+++ various/github-language-popularity/main.go
@@ -36,7 +36,7 @@ var mostpop = regexp.MustCompile("is <strong>the most")
 
 func popularity(lang string) int {
     url := "http://github.com/languages/" + lang
-    println(url)
+    //~ println(url)
     resp, err := http.Get(url)
     if err != nil {panic(err)}
     body, err := ioutil.ReadAll(resp.Body)
@@ -75,7 +75,7 @@ func main() {
                 return
             }
             lp := <-rankch
-            println(lp.Rank, lp.Lang)
+            //~ println(lp.Rank, lp.Lang)
             ranks[lp.Rank] = lp.Lang
             nlangs--
         }
@@ -86,53 +86,3 @@ func main() {
         println(r, plang)
     }
 }
-
-/*
-#!/usr/bin/env python3.2
-
-import logging, urllib.request, urllib.parse, re, concurrent.futures, itertools
-
-def language_popularity(lang):
-    qlang = urllib.parse.quote(lang)
-    try:
-        sub = urllib.request.urlopen("http://github.com/languages/" + qlang).read()
-    except:
-        logging.exception('Error fetching %r', lang)
-        return
-    match = re.search(b'is the #(\\d+)', sub)
-    if match:
-        rank = int(match.group(1))
-    else:
-        rank = 1
-    return rank, lang
-
-def languages():
-    for match in re.finditer(b'/languages/([^/]+?)"', urllib.request.urlopen('http://github.com/languages').read()):
-        yield urllib.parse.unquote(match.group(1).decode())
-
-def main():
-    futures = []
-    with concurrent.futures.ThreadPoolExecutor(45) as pool:
-        for lang in languages():
-            futures.append(pool.submit(language_popularity, lang))
-        ranks = {}
-        completed = concurrent.futures.as_completed(futures)
-        for rank in itertools.count(1):
-            while rank not in ranks:
-                try:
-                    result = next(completed).result()
-                except StopIteration:
-                    break
-                if result is None:
-                    continue
-                new_rank, lang = result
-                ranks[new_rank] = lang
-            if rank not in ranks:
-                break
-            print(rank, ranks[rank])
-        for rank1 in sorted(r for r in ranks if r >= rank):
-            print(rank1, ranks[rank1])
-
-if __name__ == '__main__':
-    main()
-*/
