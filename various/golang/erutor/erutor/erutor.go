c1e9b75336a5b53c6975d82a75f82f65df5ab8e3
Matt Joiner <anacrolix@gmail.com>
2011-12-23 22:15:06 +1100

    Save progress on golang/erutor.

--- various/golang/erutor/erutor/erutor.go
+++ various/golang/erutor/erutor/erutor.go
@@ -9,9 +9,10 @@ import (
 
 const block_size = 1 << 14
 
-func newTorrent(metainfo bencode.Dict) *torrent {
-    t := &torrent{Metainfo: metainfo}
+func newTorrent(metainfo bencode.Dict) *Torrent {
+    t := &Torrent{Metainfo: metainfo}
     t.numberOfPieces = uint(len(t.Metainfo["info"].(bencode.Dict)["pieces"].(string)) / 20)
+    url :=
     t.trackers = append(t.trackers, newTracker(metainfo["announce"].(string)))
     announce_list := metainfo["announce-list"].(bencode.List)
     for _, tier_list := range announce_list {
@@ -26,16 +27,48 @@ type block struct {
     begin, length int
 }
 
-type torrent struct {
+type Torrent struct {
     Metainfo bencode.Dict
     wanted [][]uint
     numberOfPieces uint
     blocksPerPiece uint
-    trackers []Tracker
+    trackers map[string]Tracker
+    infoHash [20]byte
+    peerId [20]byte
 }
 
-func (t torrent) Run() {
+func (t Torrent) Run() {
+    for _, track := range t.trackers {
+        track.Announce(t)
+    }
 }
+
+func (t *Torrent) InfoHash() []byte {
+    return t.infoHash[:]
+}
+
+func (t *Torrent) PeerId() []byte {
+    return t.peerId[:]
+}
+
+func (t *Torrent) Uploaded() int {
+    return 0
+}
+
+func (t *Torrent) Downloaded() int {
+    return 0
+}
+
+func (t *Torrent) Left() int {
+    return 0
+}
+
+func (t *Torrent) Port() int {
+    return 0
+}
+
+func AddTracker(url string) bool {
+
 
 func main() {
     flag.Parse()
